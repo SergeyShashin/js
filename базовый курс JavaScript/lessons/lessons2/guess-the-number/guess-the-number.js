4c0b58a4545aa72ef83be660e014cee8da9a1525
SergeyShashin <s9997910@yandex.ru>
2022-04-21 23:55:07 +0300

    guess-the-number.js done

---
+++ базовый курс JavaScript/lessons/lessons2/guess-the-number/guess-the-number.js
@@ -0,0 +1,49 @@
+'use strict';
+
+guessTheNumber();
+
+/**
+ * Отгадывание, введенного числа
+ */
+function guessTheNumber() {
+  let randomNumber = Math.round(Math.random() * 101);
+  console.log(`Сегенерированное число ${randomNumber}`);
+
+  let userInputNumber = inputNumber();
+  console.log(`Наконец-то у нас есть число ${userInputNumber} для определения совпадения `);
+
+  while (randomNumber !== userInputNumber) {
+    if (randomNumber < userInputNumber) {
+      console.log(`${userInputNumber} больше загаданного`);
+      userInputNumber = inputNumber();
+    } else {
+      console.log(`${userInputNumber} меньше загаданного`);
+      userInputNumber = inputNumber();
+    }
+  }
+
+  alert('Отгадали!');
+
+  confirm('Ещё разок?') ? guessTheNumber() : alert('До встречи!')
+}
+
+/**
+ * Возвращает число, введённое пользователем
+ * @return {number}
+ */
+function inputNumber() {
+  let userInputNumber = +prompt('Введите число от 0 до 100', 50);
+  while (!validationOfNumber(userInputNumber)) {
+    userInputNumber = inputNumber();
+  }
+  return userInputNumber
+}
+
+/**
+ * Возвращает true, если number - число от 0 до 100. False, если нет.
+ * @param {number} number 
+ * @return {boolean} 
+ */
+function validationOfNumber(number) {
+  return (number >= 0 && number <= 100) && Number.isInteger(number) && number != ''
+}
