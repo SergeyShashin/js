77a57e337b91bb0b7c7af883552e668f643a8f08
SergeyShashin <s9997910@yandex.ru>
2022-04-30 21:05:09 +0300

    Dz4 task3 done

---
+++ базовый курс JavaScript/dz/dz4/3 task/millionaire.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/*
+3**. На базе игры (приняв за пример), созданной на уроке, реализовать игру «Кто хочет стать миллионером?».
+Т.е. у вас должен быть главный объект содержащий всю логику игры, который будет иметь методы, например
+метод run, возможно метод init и т.д.
+В игре должны быть заранее подготовлены список вопросов и ответов (как минимум 5 вопросов).
+Игра должна приветствовать пользователя, после чего задавать вопросы пользователю и предлагать варианты
+ответов в виде теста, например:
+Сколько букв в слове "привет":
+a. Пять.
+b. Шесть.
+c. Семь.
+d. Куда я попал?
+Проверять правильный вариант выбрал пользователь или нет, необходимо вести счет.
+По окончании игры, когда было задано 5 вопросов, вы должны сообщить пользователю его счет и предложить
+сыграть снова.
+Также должна быть возможность выхода из игры заранее, если пользователю надоело играть.
+*/
+
+const questions = {
+  question1: 'Вопрос №1. Какой номер вопроса? \n',
+  question2: 'Вопрос №2. Какой номер вопроса? \n',
+  question3: 'Вопрос №3. Какой номер вопроса? \n',
+  question4: 'Вопрос №4. Какой номер вопроса? \n',
+  question5: 'Вопрос №5. Какой номер вопроса? \n',
+};
+
+const corectAnswers = {
+  question1: 1,
+  question2: 2,
+  question3: 3,
+  question4: 4,
+  question5: 5,
+};
+
+const game = {
+  questions,
+  corectAnswers,
+  quantityCorrectAnswers: null,
+
+  run() {
+
+    while (true) {
+      this.quantityCorrectAnswers = 0;
+
+      for (let key in questions) {
+        let answerUser;
+
+        while (true) {
+          let possibleVariants = [1, 2, 3, 4, 5];
+          answerUser = +prompt(`${questions[key]}Варианты ответа: \n 1. 1 \n 2. 2 \n 3. 3 \n 4. 4`);
+          if (possibleVariants.includes(answerUser)) {
+            break;
+          } else {
+            continue;
+          }
+        }
+
+        if (answerUser === corectAnswers[key]) {
+          this.quantityCorrectAnswers++;
+          console.log(this.quantityCorrectAnswers);
+        }
+      }
+
+
+      if (confirm(`Вы ответили правильно ${this.quantityCorrectAnswers} раз(а). Играем еще?`)) {
+        continue;
+      } else {
+        alert('До следующего раза.')
+        return;
+      }
+    }
+
+  }
+};
+
+game.run();
