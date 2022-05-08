64aad976b4637e431d31ce644e7b7a9f4e483e2c
SergeyShashin <s9997910@yandex.ru>
2022-05-08 13:07:00 +0300

    dz5 task 3 done

--- базовый курс JavaScript/dz/dz5/3 task/3.js
+++ базовый курс JavaScript/dz/dz5/3 task/3.js
@@ -35,17 +35,24 @@ buttonSend.addEventListener('click', () => {
  */
 const validationForm = {
   rules: {
-    name: ['string', '>1', '<50'],
-    phone: ['number', '>1', '<50'],
-    password: ['string', '>5', '<50'],
-    repeatPassword: ['string', 'string'],
-    form: null,
-    fieldsOfForm: null
+    name: [1, 50],
+    phone: [11],
+    password: [5, 50],
+    fieldsOfForm: null,
+    mistakes: null,
   },
   run() {
     this.fieldsOfForm = document.querySelectorAll('input');
+    this.mistakes = document.querySelectorAll('.invalid-feedback');
 
+    //удаляем все элементы содержащие ошибки
+    this.mistakes.forEach(mistake => mistake.remove());
+
+    //проверяем каждый элемент формы
     this.fieldsOfForm.forEach(fieldOfForm => {
+      fieldOfForm.classList.remove('is-invalid');
+      fieldOfForm.style = '';
+
       //Сохраняем значение input
       let fieldOfFormValue = fieldOfForm.value;
 
@@ -55,25 +62,57 @@ const validationForm = {
       //Сохраняем правило для проверки
       let rule = this.rules[fieldOfFormId];
 
-      // console.log(fieldOfFormId);
-      // console.log(this.rules[fieldOfFormId]);
-      // console.log(fieldOfFormValue);
-
       switch (fieldOfFormId) {
         case 'name':
-          console.log('Проверяем name');
-          console.log(typeof(fieldOfFormValue));
-          console.log(`Количество символов ${fieldOfFormValue.length}`);
+          if (fieldOfFormValue.length >= rule[0] && fieldOfFormValue.length < rule[1]) {
+            fieldOfForm.style.border = '1px solid green';
+          }
+          else {
+            let mistakeElement = document.createElement('div');
+            mistakeElement.textContent = 'Имя - должно содержать как минимум 1 символ, не более 50 символов.';
+            mistakeElement.classList.add('invalid-feedback');
+            fieldOfForm.parentElement.appendChild(mistakeElement);
+            fieldOfForm.classList.add('is-invalid');
+          }
           return;
-          case 'phone':
-            console.log('Проверяем phone');
-            console.log(typeof(fieldOfFormValue));
+        case 'phone':
+          //Сохраняем костыль т.к. не знаю, что делать с типом данных string, приходящем из input во всех случаях
+          let crutch = fieldOfFormValue / 10;
+
+          if (fieldOfFormValue.length === rule[0] && crutch >= 0) {
+            fieldOfForm.style.border = '1px solid green';
+          }
+          else {
+            let mistakeElement = document.createElement('div');
+            mistakeElement.textContent = 'Телефон - должeн содержать 11 цифр, не больше, не меньше.';
+            mistakeElement.classList.add('invalid-feedback');
+            fieldOfForm.parentElement.appendChild(mistakeElement);
+            fieldOfForm.classList.add('is-invalid');
+          }
           return;
         case 'password':
-          console.log('Проверяем password');
+          if (fieldOfFormValue.length >= rule[0] && fieldOfFormValue.length < rule[1]) {
+            fieldOfForm.style.border = '1px solid green';
+          }
+          else {
+            let mistakeElement = document.createElement('div');
+            mistakeElement.textContent = 'Пароль - минимум 5 символов, максимум 50.';
+            mistakeElement.classList.add('invalid-feedback');
+            fieldOfForm.parentElement.appendChild(mistakeElement);
+            fieldOfForm.classList.add('is-invalid');
+          }
           return;
         case 'repeatPassword':
-          console.log('Проверяем repeatPassword');
+          if (document.getElementById('password').value === fieldOfFormValue) {
+            fieldOfForm.style.border = '1px solid green';
+          }
+          else {
+            let mistakeElement = document.createElement('div');
+            mistakeElement.textContent = 'Значение должно совпадать с полем пароль.';
+            mistakeElement.classList.add('invalid-feedback');
+            fieldOfForm.parentElement.appendChild(mistakeElement);
+            fieldOfForm.classList.add('is-invalid');
+          }
           return;
 
 
