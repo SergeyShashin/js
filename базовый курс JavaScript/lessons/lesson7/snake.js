fc8c7f28e8ff1230ebc067769a369508d781ada6
SergeyShashin <s9997910@yandex.ru>
2022-05-15 13:58:50 +0300

    Lesson7 start. Snake.

---
+++ базовый курс JavaScript/lessons/lesson7/snake.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @type {Object} settings. Настройки игры змейки.
+ * @params {number} rowsCount. К
+ */
+const settings = {
+  rowsCount: 10,
+  colsCount: 10,
+  speedSnake: 5,
+  winFoodCount: 50
+}
