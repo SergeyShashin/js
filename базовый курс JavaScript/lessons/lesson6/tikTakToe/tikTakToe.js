a6511c53ed91df8e6f75d50445e27925134f786c
SergeyShashin <s9997910@yandex.ru>
2022-05-22 15:30:02 +0300

    Lesson6. TikTakToe start.

---
+++ базовый курс JavaScript/lessons/lesson6/tikTakToe/tikTakToe.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @type {object} Игра "Крестики нолики".
+ */
+const tikTakToe = {
+  init(){
+    console.log('Старт игры "Крестики нолики');    
+  }
+
+}
+
+window.onload = tikTakToe.init();
