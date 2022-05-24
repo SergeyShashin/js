53c40a2fb0bc5ed047670de0b134ff5f9f134893
SergeyShashin <s9997910@yandex.ru>
2022-05-24 23:40:44 +0300

    Advanced course JS. Lesson1 done.

--- продвинутый курс JavaScript/lessons/lesson1/menu.js
+++ продвинутый курс JavaScript/lessons/lesson1/menu.js
@@ -1,2 +1,56 @@
-'use strict'
+'use strict';
 
+function Container(id, className) {
+  this.id = id;
+  this.className = className;
+}
+
+
+Container.prototype.render = function () {
+  var div = document.createElement('div');
+
+  div.id = this.id;
+  div.className = this.className;
+
+  return div;
+}
+
+function Menu(id, className, items) {
+  Container.call(this, id, className);
+
+  this.items = items;
+}
+
+Menu.prototype = Object.create(Container.prototype);
+Menu.prototype.render = function () {
+  var ul = document.createElement('ul');
+
+  ul.id = this.id;
+  ul.className = this.className;
+  this.items.forEach(function (item) {
+    if (item instanceof Container) {
+      ul.appendChild(item.render());
+    }
+  });
+
+  return ul;
+}
+
+function MenuItem(id, className, href, label) {
+  Container.call(this, id, className);
+  this.href = href;
+  this.label = label;
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+
+MenuItem.prototype.render = function () {
+  var a = document.createElement('a');
+  a.id = this.id;
+  a.className = this.className;
+  a.href = this.href;
+  a.textContent = this.label;
+  var li = document.createElement('li');
+  li.appendChild(a);
+  return li;
+}
