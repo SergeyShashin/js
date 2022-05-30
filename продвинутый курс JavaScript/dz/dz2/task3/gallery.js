9bc71bd92a7494631a91a3bccb9c9290153c371b
SergeyShashin <s9997910@yandex.ru>
2022-05-30 22:42:13 +0300

    Advanced courese JS. Dz2 task3 update.

--- продвинутый курс JavaScript/dz/dz2/task3/gallery.js
+++ продвинутый курс JavaScript/dz/dz2/task3/gallery.js
@@ -5,25 +5,37 @@
 на основании которого строится сетка изображений со ссылками на полноразмерные картинки.
 */
 
-var galleryElement = document.getElementById('gallery');
+function loadImg(callback) {
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://localhost:3000/gallery');
-xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://localhost:3000/gallery');
+  xhr.send();
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    var response = JSON.parse(xhr.responseText);
-    response.forEach(function (item) {
-      var img = new Image();
-      var a = document.createElement('a');
-      a.href = item.pathFullImg;
-      img.id = item.id;
-      img.src = item.pathImg;
-      a.appendChild(img);
-      document.body.appendChild(a);
-    });
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        callback(response);
+
+      }
+    }
   }
 }
 
+loadImg(function (response) {
+  var galleryElement = document.getElementById('gallery');
+  var ul = document.createElement('ul');
 
+  response.forEach(function (item) {
+    var img = new Image();
+    var a = document.createElement('a');
+    var li = document.createElement('li');
+    a.href = item.pathFullImg;
+    img.id = item.id;
+    img.src = item.pathImg;
+    li.appendChild(img);
+    a.appendChild(li);
+    ul.appendChild(a);
+    galleryElement.appendChild(ul);
+  });
+});
