df23282ab80d1f899d4cc46949c772cde1ee79ba
SergeyShashin <s9997910@yandex.ru>
2022-05-31 00:43:38 +0300

    Advanced course JS. Dz2 task4 done.

---
+++ продвинутый курс JavaScript/dz/dz2/task4/answer.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/*
+4. *Создать два статических ответа {result : “success”} и {result: “error”}. 
+В зависимости от каждого из них навесить на определенный ajax-запрос обработчик результата.
+*/
+
+function handler(callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'db.json');
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        callback(response);
+      }
+    }
+  }
+}
+
+window.onload = function () {
+  var btnSendEl = document.getElementById('btnSend');
+  btnSendEl.addEventListener('click', function () {
+    handler(function (response) {
+      console.log(response.results[0].result);
+      if (response.results[0].result === 'success') {
+        btnSendEl.className = 'success';
+      }
+      // if (response.results[1].result === 'error') {
+      //   btnSendEl.className = 'error';
+      // }
+
+    });
+  });
+
+
+
+}
