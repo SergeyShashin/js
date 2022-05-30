5257091e7a9cc87cf9aa12c75620ffbe5439aa38
SergeyShashin <s9997910@yandex.ru>
2022-05-30 22:13:33 +0300

    Advanced courese JS. Dz2 task3 done.

--- продвинутый курс JavaScript/dz/dz2/task3/gallery.js
+++ продвинутый курс JavaScript/dz/dz2/task3/gallery.js
@@ -5,176 +5,25 @@
 на основании которого строится сетка изображений со ссылками на полноразмерные картинки.
 */
 
-/**
- * @type {Object} gallery Галерея.
- * @property {Object} settings Настройки.
- */
-const gallery = {
-  /**
-   * @type {Object} settings Настройки.
-   * @property {string} idGallery id Галереи.
-   * @property {string} classGallaryWrap Класс обертки для открытия изображений большого размера.
-   * @property {string} classImgBtnWrap Класс блока с большим изображением и кнопкой закрыть.
-   * @property {string} classImgMax Класс для изображения большого размера.
-   * @property {string} classBtnClose Класс для кнопки закрыть.
-   * @property {string} imgBtnClosePath Путь к файлу с изображением кнопки закрыть.
-   * @property {string} imgNotLoadingPath Путь к файлу с запасным изображением.
-   */
-  settings: {
-    idGallery: 'gallery',
-    classGallaryWrap: 'gallery-wrap',
-    classImgBtnWrap: 'img-btn-wrap',
-    classImgMax: 'img-max',
-    classBtnClose: 'btn-close',
-    imgBtnClosePath: 'img/gallery/close.png',
-    imgNotLoadingPath: 'img/gallery/nothing.png',
-  },
-  elementGallery: null,
-  pathToImgBigSize: null,
-  galleryWrapElement: null,
-  imgBtnWrapElement: null,
-  imgMax: null,
+var galleryElement = document.getElementById('gallery');
 
-  curentOpenImgId: null,
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://localhost:3000/gallery');
+xhr.send();
 
-  /**
-   * Инициализация объекта галереи
-   */
-  init(userSettings = {}) {
-    Object.assign(this.settings, userSettings);
-    this.elementGallery = document.getElementById(this.settings.idGallery);
-    this.elementGallery.addEventListener('click', (event) => this.clickHandler(event));
-  },
-
-  /**
-   * Обработчик клика
-   * @param {NodeList} event Событие клика.
-   */
-  clickHandler(event) {
-    if (event.target.tagName !== 'IMG') {
-      return
-    }
-
-    this.curentOpenImgId = event.target.id;
-
-    this.pathToImgBigSize = event.target.dataset.fullImageUrl;
-
-    this.createGalleryWrapElement();
-
-    this.createImgBtnWrapElement();
-
-    this.createArrowLeft();
-
-    this.addImgMax();
-
-    this.createArrowRight();
-
-    this.createBtnClose();
-
-  },
-
-  /**
-   * Создаёт обертку для открытия изображений большого размера.
-   */
-  createGalleryWrapElement() {
-    this.galleryWrapElement = document.createElement('div');
-    this.galleryWrapElement.classList.add(this.settings.classGallaryWrap);
-    document.body.appendChild(this.galleryWrapElement);
-  },
-
-  /**
-   * Создаёт блок для большого изображения и кнопки закрытью
-   */
-  createImgBtnWrapElement() {
-    this.imgBtnWrapElement = document.createElement('div');
-    this.imgBtnWrapElement.classList.add(this.settings.classImgBtnWrap);
-    document.body.appendChild(this.imgBtnWrapElement);
-  },
-
-  /**
-   * Создаёт кнопку закрыть и добавляет её в блок imgBtnWrap.
-   */
-  createBtnClose() {
-    let btnClosecElement = document.createElement('button');
-    btnClosecElement.classList.add(this.settings.classBtnClose);
-    this.imgBtnWrapElement.appendChild(btnClosecElement);
-
-    let imgBtnClose = document.createElement('img');
-    imgBtnClose.src = this.settings.imgBtnClosePath;
-    btnClosecElement.appendChild(imgBtnClose);
-
-    imgBtnClose.addEventListener('click', () => this.close());
-  },
-
-  /**
-   * Создает и добавляет изображение большого размера в imgBtnWrap 
-   */
-  addImgMax() {
-    this.imgMax = document.createElement('img');
-    this.imgMax.classList.add(this.settings.classImgMax);
-    this.imgMax.src = this.pathToImgBigSize;
-    this.imgMax.onerror = () => this.imgMax.src = this.settings.imgNotLoadingPath;
-    this.imgBtnWrapElement.appendChild(this.imgMax);
-  },
-
-  /**
-   * Закрывает текущеее изображение большого размера
-   */
-  close() {
-    this.imgBtnWrapElement.remove();
-    this.galleryWrapElement.remove();
-  },
-
-  /**
-   * Создает элемент стрелки влево
-   */
-  createArrowLeft() {
-    let arrowLeft = document.createElement('div');
-    arrowLeft.classList.add('arrow-left');
-    arrowLeft.textContent = '<';
-    this.imgBtnWrapElement.appendChild(arrowLeft);
-    arrowLeft.addEventListener('click', (e) => this.clickLeft(e));
-
-  },
-
-  /**
-   * Создает элемент стрелки вправо
-   */
-  createArrowRight() {
-    let arrowRight = document.createElement('div');
-    arrowRight.classList.add('arrow-right');
-    arrowRight.textContent = '>';
-    this.imgBtnWrapElement.appendChild(arrowRight);
-    arrowRight.addEventListener('click', () => this.clickRight());
-  },
-
-  /**
-   * Обрабатывает клик влево
-   */
-  clickLeft() {
-    if (document.getElementById(this.curentOpenImgId).previousElementSibling) {
-      this.imgMax.src = document.getElementById(this.curentOpenImgId).previousElementSibling.dataset.fullImageUrl;
-      this.curentOpenImgId = document.getElementById(this.curentOpenImgId).previousElementSibling.id;
-    } else {
-      this.imgMax.src = document.getElementById(this.curentOpenImgId).parentElement.lastElementChild.dataset.fullImageUrl;
-      this.curentOpenImgId = document.getElementById(this.curentOpenImgId).parentElement.lastElementChild.id;
-    }
-  },
-
-  /**
-   * Обрабатывает клик вправо
-   */
-  clickRight() {
-    if (document.getElementById(this.curentOpenImgId).nextElementSibling) {
-      this.imgMax.src = document.getElementById(this.curentOpenImgId).nextElementSibling.dataset.fullImageUrl;
-      this.curentOpenImgId = document.getElementById(this.curentOpenImgId).nextElementSibling.id;
-    } else {
-      this.imgMax.src = document.getElementById(this.curentOpenImgId).parentElement.firstElementChild.dataset.fullImageUrl;
-      this.curentOpenImgId = document.getElementById(this.curentOpenImgId).parentElement.firstElementChild.id;
-    }
-  },
-
-
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    var response = JSON.parse(xhr.responseText);
+    response.forEach(function (item) {
+      var img = new Image();
+      var a = document.createElement('a');
+      a.href = item.pathFullImg;
+      img.id = item.id;
+      img.src = item.pathImg;
+      a.appendChild(img);
+      document.body.appendChild(a);
+    });
+  }
 }
 
-window.onload = () => gallery.init();
+
