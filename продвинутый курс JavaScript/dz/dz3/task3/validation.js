3b5f990139c861ceaaa0844db0b42f835278c701
SergeyShashin <s9997910@yandex.ru>
2022-05-31 15:00:11 +0300

    Advanced course JS. Dz3 task3 start.

---
+++ продвинутый курс JavaScript/dz/dz3/task3/validation.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/*
+3. Создать форму обратной связи с полями: Имя, Телефон, e-mail, текст, кнопка «Отправить».
+** - При нажатии на кнопку «Отправить» произвести валидацию полей следующим образом:
+
+- Имя содержит только буквы;
+
+** - Телефон подчиняется шаблону +7(000)000-0000;**
+
+** - E-mail выглядит как mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru**
+
+** - Текст произвольный;**
+** - В случае не прохождения валидации одним из полей необходимо выделять это поле красной рамкой и сообщать пользователю об ошибке.**
+*/
+
+//invalid-feedback
+// is-invalid
+// is-valid
+
+var buttonSendElement = document.getElementById('button-send');
+
+
+buttonSendElement.addEventListener('click', function (event) {
+  console.log('sdfsd');
+  event.preventDefault();
+
+})
+
+const rules = {
+  onlySymbols: '/\w{1,}/'
+
+}
+
