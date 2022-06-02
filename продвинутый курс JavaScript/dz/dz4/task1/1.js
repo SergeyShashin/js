8400614dcba18963961d53a19fb887bc50dc5a76
SergeyShashin <s9997910@yandex.ru>
2022-06-02 21:11:04 +0300

    Advanced course JS. Dz4 task1 done;

--- продвинутый курс JavaScript/dz/dz4/task1/1.js
+++ продвинутый курс JavaScript/dz/dz4/task1/1.js
@@ -4,3 +4,45 @@
 Пример – http://dimox.name/examples/universal-jquery-tabs-script/ .
 Можно использовать любую анимацию, методы show, hide и подобные. Код примера желательно не смотреть.
 */
+
+
+var tabsEl = document.querySelector('.tabs');
+
+/**
+ * Обрабатывает клики по элементам
+ */
+tabsEl.addEventListener('click', function (event) {
+  if (event.target.tagName === 'LI') {
+    clearClassActive(tabsEl);
+    setClassActive(event.target);
+    setClassActive(giveTheElementCorrespondingToThisHeading(event.target));
+  }
+})
+
+/**
+ * Убирает класс active у всех дочерних элементов
+ * @param {HTML} element 
+ */
+function clearClassActive(element) {
+  element.querySelectorAll('.active').forEach(function (child) {
+    child.classList.remove('active');
+  });
+}
+
+/**
+ * Устанавливает класс active
+ * @param {HTML} element 
+ */
+function setClassActive(element) {
+  element.classList.add('active');
+}
+
+/**
+ * Находит контент, соответствующий заголовку
+ * @param {Html} element 
+ * @return Возвращает HTML элемент
+ */
+function giveTheElementCorrespondingToThisHeading(element) {
+  var numberHeader = element.id[element.id.length - 1];
+  return document.getElementById(`content${numberHeader}`);
+}
