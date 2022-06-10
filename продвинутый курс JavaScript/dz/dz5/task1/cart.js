49ca828a35a79577e86ff73e15c75504aa647e49
SergeyShashin <s9997910@yandex.ru>
2022-06-10 23:24:00 +0300

    Advanced course Js. Dz5 task1 start.

---
+++ продвинутый курс JavaScript/dz/dz5/task1/cart.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function loadProducts(callback){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', ); 
+
+}
+
+window.onload = function(){
+  loadProducts(function(responce){
+
+  })
+}
