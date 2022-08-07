a9ff461f501fcaec7f76b99a8e0b33eb822d99ba
SergeyShashin <s9997910@yandex.ru>
2022-08-07 12:45:14 +0300

    Advanced course JS. DZ5 task1 done.

--- продвинутый курс JavaScript/dz/dz7/task1/gulpfile.js
+++ продвинутый курс JavaScript/dz/dz7/task1/gulpfile.js
@@ -1,9 +1,23 @@
 var gulp = require('gulp');
 var scss = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync')
 
 gulp.task('scss', function () {
   return gulp.src('src/scss/**/*.scss')
     .pipe(scss())
-    .pipe(gulp.dest('src/css/'));
-}
-);
+    .pipe(gulp.dest('src/css/'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: 'src',
+    }
+  })
+})
+
+gulp.task('watch', ['browserSync'], function () {
+  gulp.watch('src/scss/**/*.scss', ['scss'])
+  gulp.watch('src/*.html', browserSync.reload)
+});
