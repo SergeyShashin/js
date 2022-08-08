99af270ce139a0f8babb51d9356899a7a8121a32
SergeyShashin <s9997910@yandex.ru>
2022-08-08 19:47:21 +0300

    Added functional components.

--- react080822/src/index.js
+++ react080822/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-let element = React.createElement(
-  'h1',
-  { className: 'react-element' },
-  'Реакт работает!'
-);
+let element = <h1 className="jsx">JSX работает!</h1>
+
+let messages = ['Привет', 'Как дела?', 'Ох'];
+
+const Message = (props) => <div>{props.text}</div>
+const MessagesList = (props) => {
+  return props.messages.map(message => <Message text={message} />)
+}
 
 ReactDom.render(
-  element,
+  <MessagesList messages={messages} />,
   document.getElementById('root')
 );
