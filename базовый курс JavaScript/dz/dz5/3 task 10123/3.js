c6094327c76be9e956f718b6217a596a5592eb00
SergeyShashin <s9997910@yandex.ru>
2023-01-03 00:39:53 +0300

    JS base level. DZ5 task3 030123 update.

--- базовый курс JavaScript/dz/dz5/3 task 10123/3.js
+++ базовый курс JavaScript/dz/dz5/3 task 10123/3.js
@@ -22,3 +22,146 @@
 Пользоваться аттрибутами HTML5 запрещено, необходимо проверки реализовать с помощью
 javascript.
 */
+
+
+
+/**
+ * @type {Object} Объект, содержащий методы валидации
+ * @method length Проверяет минимальную и максимальное количество символов при вводе
+ * @method  mustContainDigits Проверяет ввод только цифр
+ * @method  fieldCompare Проверяет идентичность двух полей
+ */
+const validationMethods = {
+
+  /**
+   * Проверяет минимальную и максимальное количество символов при вводе
+   * @param {Element} field Input для проверки
+   * @param {Array} args Условия проверки
+   * @returns {null|string} Возвращает null или сообщение об ошибке
+   */
+  length(field, args) {
+
+    let valueLength = field.value.length;
+    let sign = args[0];
+    let then = args[1];
+
+    switch (sign) {
+      case '>':
+        if (!(valueLength > then)) {
+          return `Нужно ввести ${then + 1} символов.`;
+        }
+      case '>=':
+        if (!(valueLength >= then)) {
+          return `Нужно ввести ${then} символов.`;
+        }
+      case '<':
+        if (!(valueLength < then)) {
+          return `Нужно ввести ${then - 1} символов.`;
+        }
+      case '<=':
+        if (!(valueLength <= then)) {
+          return `Нужно ввести ${then} символов.`;
+        }
+      case '===':
+        if (valueLength !== then) {
+          return `Нужно ввести ${then} символов.`;
+        }
+    }
+    return null;
+  },
+
+  /**
+  * Проверяет ввод только цифр
+  * @param {Element} field Input для проверки
+  * @returns {null|string} Возвращает null или сообщение об ошибке
+  */
+  mustContainDigits(field) {
+    let fieldValue = field.value;
+
+    if (!Number.isInteger(Number(fieldValue))) {
+      return `Нужно ввести только цифры`;
+    }
+
+    return null;
+  },
+
+
+  /**
+  * Проверяет идентичность двух полей
+  * @param {Element} field Input для проверки
+  * @param {Array} args Условия проверки
+  * @returns {null|string} Возвращает null или сообщение об ошибке
+  */
+  fieldCompare(field, args) {
+    let fieldValue = field.value;
+    let fieldForCompare = args[0].value;
+    if (fieldValue !== fieldForCompare) {
+      return `Поля не совпадают`
+    }
+
+    return null;
+  }
+}
+
+/**
+ * @type {Object} Содержит свойства и методы для проверки формы перед отправкой
+ */
+const form = {
+  formElement: null,
+  rules: null,
+
+  init() {
+
+    this.formElement = document.getElementById('formDz5Task3');
+    this.formElement.addEventListener('submit', (event) => this.formSubmit(event));
+    this.rules = [
+      {
+        selector: document.getElementById('name'),
+        methods: [
+          { name: 'length', args: ['>=', 1] },
+          { name: 'length', args: ['<=', 50] }
+        ],
+      },
+      {
+        selector: document.getElementById('phone'),
+        methods: [
+          { name: 'length', args: ['===', 11] },
+          { name: 'mustContainDigits' }
+        ]
+      },
+      {
+        selector: document.getElementById('password'),
+        methods: [  
+          { name: 'length', args: ['>=', 5] },
+          { name: 'length', args: ['<=', 50] }
+        ]
+      },
+      {
+        selector: document.getElementById('passwordRepeat'),
+        methods: [ 
+          { name: 'fieldCompare', args: [document.getElementById('password')] },
+        ]
+      }
+    ]
+  },
+
+  formSubmit(event) {
+    if (!this.validate()) {
+      event.preventDefault();
+    }
+  },
+
+  validate() {
+
+    for (let rule of this.rules) {
+      console.log(rule.name)
+      // for(let method of rule.methods){
+      //   validationMethodsp[method](rule.selector, )
+      // } 
+
+    }
+
+  }
+}
+
+form.init();
