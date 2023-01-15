a9ef0cd3607410306c700bd163930254b6e16908
SergeyShashin <s9997910@yandex.ru>
2023-01-15 20:23:24 +0300

    JS base level. Lesson7. Snake 110123 updated.

--- базовый курс JavaScript/lessons/lesson7/snake 110123/snake.js
+++ базовый курс JavaScript/lessons/lesson7/snake 110123/snake.js
@@ -1,1 +1,76 @@
 'use strict';
+
+const settings = {
+  rowsCount: 21,
+  colsSount: 21,
+  speed: 5,
+  winFoodCount: 10,
+};
+
+const config = {
+  settings,
+
+  init(userSettings) {
+    this.settings = Object.assign(this.settings, userSettings);
+  },
+
+  getRowsCount() {
+    return this.settings.rowsCount;
+  },
+
+  getColsCount() {
+    return this.settings.colsCount;
+  },
+
+  getSpeed() {
+    return this.settings.speed;
+  },
+
+  getWinFoodCount() {
+    return this.settings.speed;
+  },
+
+  validate() {
+
+  },
+
+};
+
+const status = {
+  condition: null,
+  isPlay() {
+    return this.condition === 'play';
+  },
+  isStop() {
+    return this.condition === 'stop';
+  },
+
+};
+
+const map = {
+
+};
+
+const snake = {
+
+};
+
+const food = {
+
+};
+
+const game = {
+  config,
+  status,
+  map,
+  snake,
+  food,
+
+  init(userSettings = {}) {
+    this.config.init(userSettings);
+
+  },
+
+};
+
+window.onload = game.init({ speed: 3 });
