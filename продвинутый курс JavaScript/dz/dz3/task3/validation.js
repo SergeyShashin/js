9ac382bea97585a14932b2ec1b064a868a73236b
SergeyShashin <s9997910@yandex.ru>
2023-01-23 16:01:59 +0300

    JS advanced level. DZ3. task3 230123 updated.

--- продвинутый курс JavaScript/dz/dz3/task3/validation.js
+++ продвинутый курс JavaScript/dz/dz3/task3/validation.js
@@ -12,5 +12,69 @@
 
 ** - Текст произвольный;**
 ** - В случае не прохождения валидации одним из полей необходимо выделять это поле красной рамкой и сообщать пользователю об ошибке.**
-
 */
+
+var buttonSendElement = document.getElementById('button-send');
+
+buttonSendElement.addEventListener('click', function (event) {
+  event.preventDefault();
+  var inputsEl = document.querySelectorAll('input');
+  inputsEl.forEach(function (inputEl) {
+
+    switch (inputEl.id) {
+      case 'name':
+        if (/^[a-zA-Zа-яА-яЁё]+$/.test(inputEl.value)) {
+          inputEl.classList.remove('is-invalid');
+          inputEl.classList.add('is-valid');
+        } else {
+          inputEl.classList.remove('is-valid');
+          inputEl.classList.add('is-invalid');
+          if (!document.getElementById('nameError')) {
+            var nameErrorEl = document.createElement('div');
+            nameErrorEl.id = 'nameError';
+            nameErrorEl.textContent = 'Имя может содержать только буквы';
+            nameErrorEl.className = 'invalid-feedback';
+            inputEl.parentElement.appendChild(nameErrorEl);
+          }
+        }
+        break;
+      case 'phone':
+        if (/^\+\d\(\d{3}\)\d{3}\-\d{4}$/.test(inputEl.value)) {
+          inputEl.classList.remove('is-invalid');
+          inputEl.classList.add('is-valid');
+        } else {
+          inputEl.classList.remove('is-valid');
+          inputEl.classList.add('is-invalid');
+          if (!document.getElementById('phoneError')) {
+            var phoneErrorEl = document.createElement('div');
+            phoneErrorEl.id = 'phoneError';
+            phoneErrorEl.textContent = 'Телефон подчиняется шаблону +7(000)000-0000';
+            phoneErrorEl.className = 'invalid-feedback';
+            inputEl.parentElement.appendChild(phoneErrorEl);
+          }
+        }
+        break;
+      case 'email':
+        if (/^\w+\-?\w*\.?\w*\@\w*\.\w{2,3}$/.test(inputEl.value)) {
+          inputEl.classList.remove('is-invalid');
+          inputEl.classList.add('is-valid');
+        } else {
+          inputEl.classList.remove('is-valid');
+          inputEl.classList.add('is-invalid');
+          if (!document.getElementById('emailError')) {
+            var emailErrorEl = document.createElement('div');
+            emailErrorEl.id = 'emailError';
+            emailErrorEl.textContent = 'E-mail выглядит как mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru';
+            emailErrorEl.className = 'invalid-feedback';
+            inputEl.parentElement.appendChild(emailErrorEl);
+          }
+        }
+        break;
+    }
+
+  })
+
+})
+
+
+
