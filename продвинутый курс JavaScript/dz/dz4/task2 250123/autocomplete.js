911dcc1f80361043c99030aee881111889e079d8
SergeyShashin <s9997910@yandex.ru>
2023-01-25 22:18:18 +0300

    JS advanced level. DZ4. task2 250123 done.

--- продвинутый курс JavaScript/dz/dz4/task2 250123/autocomplete.js
+++ продвинутый курс JavaScript/dz/dz4/task2 250123/autocomplete.js
@@ -5,29 +5,27 @@
 Сам список должен загружаться после загрузки страницы через AJAX.
 */
 
-function loadTowns(callback) {
-  var xhr = new XMLHttpRequest();
+
+function loadJson(callback) {
+  var xhr = new XMLHttpRequest;
   xhr.open('GET', 'http://localhost:3000/towns');
   xhr.send();
+
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
-        var responce = JSON.parse(xhr.responseText);
-        callback(responce);
+        callback(JSON.parse(xhr.responseText));
       }
     }
   }
 }
 
-window.onload = function () {
-  loadTowns(function (responce) {
-    responce.forEach(function (town) {
-      var townsElement = document.getElementById('towns');
-      var townElement = document.createElement('option');
-      townElement.textContent = town;
-      townElement.value = town;
-      townsElement.appendChild(townElement);
-    });
-
-  })
-}
+loadJson(function (towns) {
+  var selectEl = document.getElementById('towns')
+  towns.forEach(function (town) {
+    var optionEl = document.createElement('option');
+    optionEl.textContent = town;
+    optionEl.id = town;
+    selectEl.appendChild(optionEl);
+  });
+})
