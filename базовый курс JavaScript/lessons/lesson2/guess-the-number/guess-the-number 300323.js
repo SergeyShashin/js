bc889757ffe4a13d24856c22808a15cea614262c
SergeyShashin <s9997910@yandex.ru>
2023-03-31 14:05:57 +0300

    js. lesson2 30.03.23

---
+++ базовый курс JavaScript/lessons/lesson2/guess-the-number/guess-the-number 300323.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let randomNumber, attempt;
+resetGame();
+tryGuesNumber();
+
+while (confirm('Ещё?')) {
+  resetGame();
+  tryGuesNumber();
+}
+
+
+/**
+ * Устанавливает стартовые значения.
+ */
+function resetGame() {
+  randomNumber = Math.floor(Math.random() * 100);
+  attempt = 0;
+  console.log('Рандомное число' + randomNumber);
+}
+
+/**
+ * Ввод числа пользователем.
+ * @returns {number} Число, которое ввёл пользователь.
+ */
+function inputNumber() {
+  return Number(prompt('Введите целое число от 0 до 100.'));
+}
+
+/**
+ * Ведете диалог с пользователем для отгадывания загаданного числа.
+ * 
+ */
+function tryGuesNumber() {
+  let userNumber = inputNumber();
+
+  while (userNumber < 0 || userNumber > 100 || !Number.isInteger(userNumber) || Number.isNaN()) {
+    userNumber = inputNumber();
+  }
+
+  if (randomNumber === userNumber) {
+    console.log(`Отгадали c ${attempt} попытки.`);
+    return
+  } else if (randomNumber > userNumber) {
+    console.log(`Попробуйте число больше ${userNumber}.`);
+    attempt++;
+  } else if (randomNumber < userNumber) {
+    console.log(`Попробуйте число меньше. ${userNumber}`);
+    attempt++;
+  }
+
+  tryGuesNumber();
+}
+
+
