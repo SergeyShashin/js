8fee8d6530a481a5bbd17dc4105192fbf8848f31
SergeyShashin <s9997910@yandex.ru>
2023-04-04 21:21:44 +0300

    js. dz4 task1 done.

--- базовый курс JavaScript/dz/dz4/1 task 040423/1.js
+++ базовый курс JavaScript/dz/dz4/1 task 040423/1.js
@@ -20,26 +20,24 @@ thirdDigit: 0,
 необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
 */
 
-let number = +prompt(`Введите целое число в диапазоне [0, 999]`);
+console.log(numberToObject(Number(prompt('Введите число в диапазоне [0, 999]'))));
 
-console.log(transferNumberInToObject(number));
-
-function transferNumberInToObject(number) {
-  const obj = {};
-
-  if (!validateNumber(number)) {
-    console.error(`Для преобразования нужно передать целое число в диапазоне [0, 999].`);
-  } else {
-    number = number.toString();
-    // console.log(number);
-    obj.firstDigit = number[0];
-    obj.secondDigit = number[1];
-    obj.thirdDigit = number[2];
+/**
+ * Преобразующует число в объект.
+ * @param {number} number  Число в диапазоне [0,999]
+ * @returns {Object} Возвращает объект, в котором в соответствующих свойствах описаны
+ * разряды числа или пустой объект.
+ */
+function numberToObject(number) {
+  
+  if (!Number.isInteger(number) || number < 0 || number > 999) {
+    return {}
   }
 
-  return obj;
-}
+  return {
+    firstDigit: parseInt(number / 100),
+    secondDigit: parseInt(number / 10 % 10),
+    thirdDigit: number % 10,
+  }
 
-function validateNumber(number) {
-  return number >= 0 && number <= 999 && Number.isInteger(number);
 }
