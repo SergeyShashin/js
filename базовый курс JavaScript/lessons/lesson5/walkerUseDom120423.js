b95000151d9d39576c0e1572ae6c1ef071df3c7b
SergeyShashin <s9997910@yandex.ru>
2023-04-12 16:25:45 +0300

    lesson5 120423 updated.

--- базовый курс JavaScript/lessons/lesson5/walkerUseDom120423.js
+++ базовый курс JavaScript/lessons/lesson5/walkerUseDom120423.js
@@ -1,3 +1,82 @@
 'use strict';
 
-console.log('работает');
+
+/**
+ * @type {Object} Объект с настройками.
+ * @property {int} rowsCount Количество строк.
+ * @property {int} colsCount Количество колонок.
+ * @property {int} startPositionPlayerX Стартовая позиция игрока по оси x.
+ * @property {int} startPositionPlayerY Стартовая позиция игрока по оси y.
+ */
+const settings = {
+  rowsCount: 10,
+  colsCount: 10,
+  startPositionPlayerX: 5,
+  startPositionPlayerY: 5,
+  startDirection: 'right'
+}
+
+
+/**
+ * @type {Object} Объект игрока.
+ * @property {int} x Положение игрока по оси x.
+ * @property {int} y Положение игрока по оси y.
+ * @property {string} direction Направление игрока. 
+ * @method init Устанавливает игрока в стартовое положение. 
+ */
+const player = {
+  x: null,
+  y: null,
+  direction: null,
+
+  /**
+   * Устанавливает игрока в стартовое положение.
+   * @param {*} startX Положение игрока по оси x.
+   * @param {*} startY  Положение игрока по оси y.
+   * @param {*} direction Направление игрока.
+   */
+  init(startX, startY, direction) {
+    this.x = startX;
+    this.y = startY;
+    this.direction = direction;
+  },
+
+  /**
+   * Возвращает координаты точки, где будет игрок, если сделает ход.
+   * @param {string} direction Направление игрока. 
+   * @returns {object} Кординаты точки, где будет игрок, если сделает ход.
+   */
+  getNextPosition(direction) {
+    switch (direction) {
+      case 'right':
+        return { x: this.x++, y: this.y }
+      case 'left':
+        return { x: this.x--, y: this.y }
+      case 'top':
+        return { x: this.x, y: this.y++ }
+      case 'bottom':
+        return { x: this.x, y: this.y-- }
+      default:
+        return console.error(`Ошибочное направление ${direction}`);
+    }
+  },
+
+  /**
+   * Перемещается в нужную позицию. 
+   * @param {Object} position 
+   */
+  move(position) {
+    this.x = position.x;
+    this.y = position.y;
+  }
+}
+
+/**
+ * @type {Object} Объект игры.
+ */
+const game = {
+  run(){
+
+  }
+}
+
