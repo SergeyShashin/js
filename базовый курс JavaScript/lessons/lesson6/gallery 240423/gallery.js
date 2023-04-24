791f57f7824c576d473be85525215a0bf3c97c9e
SergeyShashin <s9997910@yandex.ru>
2023-04-24 17:06:04 +0300

    js lesson6 gallery 240423 updated.

--- базовый курс JavaScript/lessons/lesson6/gallery 240423/gallery.js
+++ базовый курс JavaScript/lessons/lesson6/gallery 240423/gallery.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const settings = {
+  galleryElementId: 'gallery',
+  displayElementClass: 'display',
+  buttonClosePathImg: 'img/gallery/close.png',
+}
+
+const gallery = {
+  settings,
+  galleryElement: null,
+
+  init(userSettings = {}) {
+    Object.assign(this.settings, userSettings);
+    this.galleryElement = document.getElementById(this.settings.galleryElementId);
+
+    this.galleryElement.addEventListener('click', (e) => this.handlerClick(e));
+  },
+
+  handlerClick(e) {
+    let targetClick = e.target.tagName;
+
+
+    if (e.target.tagName !== 'IMG') {
+      return
+    }
+
+    let srcFullImgUrl = targetClick.dataset.fullImageUrl;
+    
+
+  }
+
+
+
+}
+
+gallery.init();
