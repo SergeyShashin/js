4c7b625164fae2222fcf5931b6e0e64738652087
SergeyShashin <s9997910@yandex.ru>
2023-04-24 22:55:49 +0300

    js lesson6 240423 done

--- базовый курс JavaScript/lessons/lesson6/gallery 240423/gallery.js
+++ базовый курс JavaScript/lessons/lesson6/gallery 240423/gallery.js
@@ -1,15 +1,27 @@
 'use strict';
 
+/**
+ * @type {Object} Настройки галереи
+ */
 const settings = {
   galleryElementId: 'gallery',
-  displayElementClass: 'display',
-  buttonClosePathImg: 'img/gallery/close.png',
+  displayElementId: 'display',
+  closeElementPathImg: 'img/gallery/close.png',
+  closeId: 'close',
+  wrapId: 'wrap'
 }
 
+/**
+ * @type {Object} Галерея
+ */
 const gallery = {
   settings,
   galleryElement: null,
 
+  /**
+   * Инициализация галереи
+   * @param {Object} userSettings Пользовательские настройки 
+   */
   init(userSettings = {}) {
     Object.assign(this.settings, userSettings);
     this.galleryElement = document.getElementById(this.settings.galleryElementId);
@@ -17,20 +29,69 @@ const gallery = {
     this.galleryElement.addEventListener('click', (e) => this.handlerClick(e));
   },
 
+  /**
+   * Открывает изображение в полном разрешении
+   * @param {Event} e Событие клика по галерее 
+   */
   handlerClick(e) {
-    let targetClick = e.target.tagName;
-
 
     if (e.target.tagName !== 'IMG') {
       return
     }
 
-    let srcFullImgUrl = targetClick.dataset.fullImageUrl;
-    
+    let srcFullImgUrl = e.target.dataset.fullImageUrl;
+    this.openFullImg(srcFullImgUrl);
+  },
 
+  /**
+   * Открывает дисплей с картинкой в полном разрешении
+   * @param {string} src Путь к картинке 
+   */
+  openFullImg(src) {
+    let displayElement = this.getDisplay(src);
+    document.body.appendChild(displayElement);
+  },
+
+  /**
+   * Возвращает или создаёт дисплей
+   * @param {string} src Путь к картинке
+   */
+  getDisplay(src) {
+    let displayElement = document.getElementById(this.settings.displayElementId);
+
+    if (displayElement) {
+      return displayElement;
+    }
+
+    displayElement = document.createElement('div');
+    displayElement.id = this.settings.displayElementId;
+
+    let closeElement = new Image();
+    closeElement.id = this.settings.closeId;
+    closeElement.src = this.settings.closeElementPathImg;
+    displayElement.appendChild(closeElement);
+    closeElement.addEventListener('click', () => this.close(displayElement));
+
+    let wrapElement = document.createElement('div');
+    wrapElement.id = this.settings.wrapId;
+
+    let fullImgElement = new Image();
+    fullImgElement.src = src;
+    wrapElement.appendChild(fullImgElement);
+
+    displayElement.appendChild(wrapElement);
+
+
+    return displayElement;
+  },
+
+  /**
+   * Закрывает дисплей с картинкой
+   * @param {HTMLElement} displayElement Дисплей 
+   */
+  close(displayElement) {
+    displayElement.remove();
   }
-
-
 
 }
 
