9f46fd743fa9fa2acab5c74d3ceb6562c6706615
SergeyShashin <s9997910@yandex.ru>
2023-05-07 21:13:05 +0300

    JS. Advanced level. Lesson1 070503 done.

---
+++ продвинутый курс JavaScript/lessons/lesson1/menu070523/menu.js
@@ -0,0 +1,49 @@
+
+function Container(idParam, classNameParam) {
+  this.idParam = idParam;
+  this.classNameParam = classNameParam;
+}
+
+Container.prototype.render = function () {
+  var div = document.createElement('div');
+  div.id = this.idParam;
+  div.className = this.classNameParam;
+  return div;
+}
+
+function Menu(idParam, classNameParam, items) {
+  Container.call(this, idParam, classNameParam);
+  this.items = items;
+}
+
+Menu.prototype = Object.create(Container.prototype);
+Menu.prototype.render = function () {
+  var ul = document.createElement('ul');
+  ul.id = this.idParam;
+  ul.className = this.classNameParam;
+  this.items.forEach(function (item) {
+    console.log(item);
+    console.log(item instanceof Container);
+    if (item instanceof Container) {
+      ul.appendChild(item.render());
+    }
+  });
+
+  return ul;
+}
+
+function ItemsMenu(href, label) {
+  Container.call(this, '', 'items');
+  this.href = href;
+  this.label = label;
+}
+
+ItemsMenu.prototype = Object.create(Container.prototype);
+ItemsMenu.prototype.render = function () {
+  var li = document.createElement('li');
+  var a = document.createElement('a');
+  a.href = this.href;
+  a.textContent = this.label
+  li.appendChild(a);
+  return li;
+}
