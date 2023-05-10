3d7f2f533895dac87abfb8c5f0305f533eb4ead7
SergeyShashin <s9997910@yandex.ru>
2023-05-10 07:37:50 +0300

    JS. Advanced level. DZ1 task3 updated.

--- продвинутый курс JavaScript/dz/dz1/task3 090523/burger.js
+++ продвинутый курс JavaScript/dz/dz1/task3 090523/burger.js
@@ -12,3 +12,27 @@
 Напишите программу, рассчитывающую стоимость и калорийность гамбургера. Используйте ООП-подход 
 (подсказка: нужен класс Гамбургер, константы, методы для выбора опций и расчета нужных величин).
 */
+
+function Hamburger(size, stuffing) {
+  this.cost = 0;
+  this.calories = 0;
+  console.log('Новый гамбургер');
+  switch (size) {
+    case 'SIZE_SMALL':
+      this.cost += 50;
+      this.calories += 20;
+  }
+}
+
+Hamburger.prototype.getCost = function(){
+  return this.cost;
+}
+Hamburger.prototype.getCalories = function(){
+  return this.calories;
+}
+
+var HAMBURGER_SIZE_SMAll = 0;
+var HAMBURGER_SIZE_BIG = 1;
+var STUFFING_CHEESE = 2;
+var STUFFING_SALAD = 3;
+var STUFFING_POTATO = 4;
