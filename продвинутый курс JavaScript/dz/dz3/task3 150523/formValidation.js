116b37be0340db066a4ed8d64b9abe4c905a0fc3
SergeyShashin <s9997910@yandex.ru>
2023-05-16 13:40:16 +0300

    JS. Advanced level. DZ3 task3 150523 done.

--- продвинутый курс JavaScript/dz/dz3/task3 150523/formValidation.js
+++ продвинутый курс JavaScript/dz/dz3/task3 150523/formValidation.js
@@ -8,3 +8,67 @@
 // ** - Текст произвольный;**
 // ** - В случае не прохождения валидации одним из полей необходимо выделять это поле красной рамкой и сообщать пользователю об ошибке.**
 
+var formContactDataElement = document.getElementById('formContactData');
+var btnSend = document.getElementById('send');
+
+var formValidationMethods = {
+  mustContainLeters(content) {
+    return /^[a-zа-яё]+$/i.test(content);
+  },
+  checkPhone(content) {
+    return /^\+\d\(\d{3}\)\d{3}\-\d{4}$/i.test(content);
+  },
+  checkEmail(content) {
+    return /^\d?\w+\d?\-?\d?\@\w+\d?\.\w{2,4}$/i.test(content);
+  }
+}
+
+btnSend.addEventListener('click', function (event) {
+  event.preventDefault();
+  checkForm();
+});
+
+function checkForm() {
+  var isValid = true;
+  formContactDataElement.querySelectorAll('input').forEach(function (element) {
+
+    var content = element.value;
+
+    switch (element.id) {
+      case 'name':
+        if (formValidationMethods.mustContainLeters(content)) {
+          element.className = 'ok';
+        } else {
+          element.className = 'error';
+          alert('Поле "Имя" должно содержать только буквы');
+          isValid = false;
+        }
+        break;
+      case 'phone':
+        if (formValidationMethods.checkPhone(content)) {
+          element.className = 'ok';
+        } else {
+          element.className = 'error';
+          alert('Поле "Телефон" нужно заполнить по шаблону +7(000)000-0000');
+          isValid = false;
+        }
+        break;
+      case 'email':
+        if (formValidationMethods.checkEmail(content)) {
+          element.className = 'ok';
+        } else {
+          element.className = 'error';
+          alert('Поле "Емейл" нужно заполнить по шаблону mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru');
+          isValid = false;
+        }
+        break;
+    }
+
+  });
+
+  if (isValid) {
+    formContactDataElement.submit();
+    alert('Отправлено');
+  }
+
+}
