facb30a56fbeb58d8c45c57b1b5b510aab2f543a
SergeyShashin <s9997910@yandex.ru>
2023-05-20 23:02:34 +0300

    JS. Advanced level. DZ4 task1 200523 done.

---
+++ продвинутый курс JavaScript/dz/dz4/task1 20.05.23/1.js
@@ -0,0 +1,21 @@
+'use strict';
+/*
+1) С помощью jQuery создать контрол, работающий с вкладками.
+Пример – http://dimox.name/examples/universal-jquery-tabs-script/ .
+Можно использовать любую анимацию, методы show, hide и подобные. Код примера желательно не смотреть.
+*/
+
+(function ($) {
+  $('li').on('click', function () {
+    clearClassActive();
+    var currentElement = $(this);
+    var currentContentId = currentElement[0].id;
+    currentElement.addClass('active');
+    $('#content' + currentContentId.slice(-1)).addClass('active');
+  });
+
+  function clearClassActive() {
+    $('.active').removeClass('active');
+  }
+
+})(jQuery)
