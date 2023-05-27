79dc47a58717b3e1024d8debfad36a724db22249
SergeyShashin <s9997910@yandex.ru>
2023-05-27 18:06:30 +0300

    JS. Advanced level. DZ5 task1 270523 updated.

--- продвинутый курс JavaScript/dz/dz5/task1 270523/cart.js
+++ продвинутый курс JavaScript/dz/dz5/task1 270523/cart.js
@@ -1,3 +1,74 @@
 'use strict';
 
 //Модуль корзины. Добавление, удаление товаров.
+
+var productsListEl = document.getElementById('productsList');
+var cartEl = document.getElementById('cart');
+
+buildList();
+
+function getData(method, url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        callback(JSON.parse(xhr.responseText));
+      }
+    }
+  }
+}
+
+function buildList() {
+  productsListEl.innerHTML = '';
+  getData('GET', 'http://localhost:3000/products', function (data) {
+    data.forEach(function (item) {
+      var liEl = document.createElement('li');
+      liEl.textContent = item.name + ' ' + item.price;
+      var btnBuy = document.createElement('button');
+      btnBuy.textContent = 'Купить';
+      btnBuy.id = item.id;
+      btnBuy.dataset.name = item.name;
+      btnBuy.dataset.price = item.price;
+      productsListEl.appendChild(liEl);
+      liEl.appendChild(btnBuy);
+    });
+  });
+}
+
+// (
+//   function ($) {
+//     var productsListEl = document.getElementById('productsList');
+//     var cartEl = document.getElementById('cart');
+
+//     buildList();
+
+//     function getData(method, url, callback) {
+//       $.ajax({
+//         url: url,
+//         success: function(data){
+//           callback(data);
+//         }
+//       })
+//     }
+
+//     function buildList() {
+//       getData('GET', 'http://localhost:3000/products', function (data) {
+//         data.forEach(function (item) {
+//           var liEl = document.createElement('li');
+//           liEl.textContent = item.name + ' ' + item.price;
+//           var btnBuy = document.createElement('button');
+//           btnBuy.textContent = 'Купить';
+//           btnBuy.id = item.id;
+//           btnBuy.dataset.name = item.name;
+//           btnBuy.dataset.price = item.price;
+//           productsListEl.appendChild(liEl);
+//           liEl.appendChild(btnBuy);
+//         });
+//       });
+//     }
+
+
+//   }
+// )(jQuery);
