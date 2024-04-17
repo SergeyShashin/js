cd3ae73166c887eceb3fb35492d14485b4985d1e
SergeyShashin <s9997910@yandex.ru>
2024-04-17 10:37:55 +0300

    JS. Base. bulls-and-cows 170424 updated.

--- базовый курс JavaScript/lessons/lesson3/bulls-and-cows 170424.js
+++ базовый курс JavaScript/lessons/lesson3/bulls-and-cows 170424.js
@@ -1,2 +1,46 @@
 'use strict';
 
+let randomNumber;
+let attemps;
+
+resetGame();
+guessNumber();
+
+/**
+ * Установка игры в начальное положение
+ */
+function resetGame() {
+  attemps = 0;
+  randomNumber = Math.floor(Math.random() * (9999 - 1000) + 1000);
+}
+
+/**
+ * Принимает число от пользователя. Проверяет наличие быков и коров. Вывводит результат.
+*/
+function guessNumber() {
+  while (true) {
+    
+    let guessNumber = Number(prompt('Введите 4-х значное число или -1 для выхода.'));
+    
+    if (guessNumber === -1) {
+      return alert('Всех благ)');
+    }
+    attemps++;
+    
+    if (guessNumber === randomNumber) {
+      sayWin();
+      return
+    }
+    
+    console.log(randomNumber);
+  }
+}
+
+function sayWin() {
+  if (confirm(`Угадали c ${attemps} попытки. Ещё?`)) {
+    resetGame();
+  } else {
+    return
+  };
+}
+
