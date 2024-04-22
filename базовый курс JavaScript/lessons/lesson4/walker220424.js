db8b888973016242c76dd4ce09f192b947596dd1
SergeyShashin <s9997910@yandex.ru>
2024-04-22 15:32:13 +0300

    JS. lesson4. walker220424 done.

--- базовый курс JavaScript/lessons/lesson4/walker220424.js
+++ базовый курс JavaScript/lessons/lesson4/walker220424.js
@@ -5,6 +5,7 @@ const settings = {
   colsCount: 10,
   charPlayer: '0 ',
   charMap: 'X ',
+  availableDirection: ['4', '6', '8', '5', 'выход']
 };
 
 const player = {
@@ -21,6 +22,20 @@ const player = {
   },
 
   move(direction) {
+    switch (direction) {
+      case '4':
+        this.x--
+        break;
+      case '6':
+        this.x++
+        break;
+      case '8':
+        this.y--
+        break;
+      case '5':
+        this.y++
+        break;
+    }
 
   }
 
@@ -33,12 +48,27 @@ const game = {
   run() {
     this.player.init({ x: Math.round(this.settings.rowsCount / 2), y: Math.round(this.settings.colsCount / 2) });
     this.render();
+    while (true) {
+      let direction = prompt(`Напечайте одно из возможных направлений ${this.settings.availableDirection}.`);
+
+      if (!this.settings.availableDirection.includes(direction)) {
+        continue
+      }
+
+      if (direction === 'выход') {
+        return
+      }
+
+      this.player.move(direction);
+      this.render();
+
+    }
   },
 
   render() {
     let map = '';
-    for (let row = 0; row < this.settings.rowsCount; row++) {
-      for (let col = 0; col < this.settings.colsCount; col++) {
+    for (let col = 0; col < this.settings.colsCount; col++) {
+      for (let row = 0; row < this.settings.rowsCount; row++) {
         console.log(this.player.getPosition());
         if (row === this.player.getPosition().x && col === this.player.getPosition().y) {
           map += this.settings.charPlayer;
