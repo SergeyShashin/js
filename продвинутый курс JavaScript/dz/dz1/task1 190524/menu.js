48f2660adcba32a2d2582e23a16e91d8f254ee84
SergeyShashin <s9997910@yandex.ru>
2024-05-19 16:30:14 +0300

    Advanced JS. DZ1 task1 190524 done.

---
+++ продвинутый курс JavaScript/dz/dz1/task1 190524/menu.js
@@ -0,0 +1,56 @@
+// 1. Улучшить базовый класс, добавив в него общий для всех метод remove(), который удаляет контейнер.
+
+function Container(someId, someClass) {
+  this.someId = someId;
+  this.someClass = someClass;
+}
+
+Container.prototype.render = function () {
+  var div = document.createElement('div');
+  div.id = this.someId;
+  div.className = this.someClass;
+  return div
+};
+
+Container.prototype.remove = function () {
+  document.getElementById(this.someId).remove();
+};
+
+function Menu(someId, someClass, items) {
+  Container.call(this, someId, someClass);
+  this.items = items;
+}
+
+Menu.prototype = Object.create(Container.prototype);
+Menu.prototype.render = function () {
+  var ul = document.createElement('ul');
+  ul.id = this.someId;
+  ul.className = this.someClass;
+  
+  this.items.forEach(item => {
+    if (item instanceof Container) {
+      ul.appendChild(item.render());
+    }
+  });
+  return ul;
+};
+
+function MenuItem(someLabel, someHref) {
+  Container.call(this, '', 'item');
+  this.someLabel = someLabel;
+  this.someHref = someHref;
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+
+MenuItem.prototype.render = function () {
+  var li = document.createElement('li');
+  var a = document.createElement('a');
+  li.id = this.someId;
+  li.className = this.someClass;
+  a.textContent = this.someLabel;
+  a.href = this.someHref;
+  li.appendChild(a);
+
+  return li
+}
