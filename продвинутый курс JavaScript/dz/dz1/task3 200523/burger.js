32b1321ba1638ded70171a89e39c6a14ba7841c6
SergeyShashin <s9997910@yandex.ru>
2024-05-21 08:44:19 +0300

    JS. Advanced. DZ1 task3 200524 updated.

--- продвинутый курс JavaScript/dz/dz1/task3 200523/burger.js
+++ продвинутый курс JavaScript/dz/dz1/task3 200523/burger.js
@@ -25,3 +25,41 @@ function handlerSubmit(e) {
 
   console.log(propsBurger);
 }
+
+function Burger(props) {
+  this.cost = 0;
+  this.numberСalories = 0;
+
+  for (var property of props) {
+    switch (property) {
+      case 'bigSize':
+        this.cost += 100;
+        this.numberСalories += 40;
+        break;
+      case 'smalSize':
+        this.cost += 50;
+        this.numberСalories += 20;
+        break;
+      case 'cheese':
+        this.cost += 10;
+        this.numberСalories += 20;
+        break;
+      case 'salad':
+        this.cost += 20;
+        this.numberСalories += 5;
+        break;
+      case 'potato':
+        this.cost += 15;
+        this.numberСalories += 10;
+        break;
+      case 'sauce':
+        this.cost += 15;
+        this.numberСalories += 0;
+        break;
+      case 'mayo':
+        this.cost += 20;
+        this.numberСalories += 5;
+        break;
+    }
+  }
+}
