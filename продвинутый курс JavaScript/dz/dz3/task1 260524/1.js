f0a7cbb5293fe6bdce97f72c14bed7171b85adb4
SergeyShashin <s9997910@yandex.ru>
2024-05-26 21:56:48 +0300

    JS. Advanced. DZ3 task1 260524 done.

---
+++ продвинутый курс JavaScript/dz/dz3/task1 260524/1.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+1. Дан большой текст, в котором для обозначения диалогов используются одинарные кавычки. 
+Придумать шаблон, который меняет одинарные кавычки на двойные.
+*/
+
+Object.values(document.getElementsByTagName('p')).map(el => el.textContent = el.textContent.replace(/(\B'|'\B)/g, '"'));
+
+
+
