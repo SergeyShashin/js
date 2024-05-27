199e9aa8e44d5ae8db9eddaea1c1a744f2c6d9f5
SergeyShashin <s9997910@yandex.ru>
2024-05-27 14:02:08 +0300

    JS. Advanced. DZ3 task3 240524 done.

--- продвинутый курс JavaScript/dz/dz3/task3 260524/validation.js
+++ продвинутый курс JavaScript/dz/dz3/task3 260524/validation.js
@@ -13,3 +13,47 @@
 ** - Текст произвольный;**
 ** - В случае не прохождения валидации одним из полей необходимо выделять это поле красной рамкой и сообщать пользователю об ошибке.**
 */
+
+var formContatsHTMLEl = document.getElementById('formContacts');
+formContatsHTMLEl.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var inputs = e.target.querySelectorAll('input');
+  var isValid = true;
+  Object.values(inputs).map(function (input) {
+    switch (input.id) {
+      case 'name':
+        if (!/^[а-яё\w]+$/i.test(input.value)) {
+          input.className = 'errorBorder';
+          isValid = false;
+          alert('Имя может содержать только буквы.');
+        } else {
+          isValid = true;
+          input.className = '';
+        }
+        break;
+      case 'phone':
+        if (!/^\+\d\(\d{3}\)\d{3}\-\d{4}$/i.test(input.value)) {
+          input.className = 'errorBorder';
+          isValid = false;
+          alert('Телефон подчиняется шаблону +7(000)000-0000');
+        } else {
+          isValid = true;
+          input.className = '';
+        }
+        break;
+      case 'email':
+        if (!/^[\w\-\.\d]+\@[\w\-\d]+\.\w{2,3}$/i.test(input.value)) {
+          input.className = 'errorBorder';
+          isValid = false;
+          alert('E-mail подчиняется шаблонам: mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru');
+        } else {
+          isValid = true;
+          input.className = '';
+        }
+        break;
+    }
+  });
+  if (isValid) {
+    alert('Форма отправлена.');
+  }
+});
