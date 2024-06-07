f25f31766a15b56dc823eca4e28fa62854759b62
SergeyShashin <s9997910@yandex.ru>
2024-06-07 12:54:30 +0300

    JS. Advanced. DZ5 task2 070624 updated.

---
+++ продвинутый курс JavaScript/dz/dz5/task2 070624/js.js
@@ -0,0 +1,28 @@
+'use stict';
+
+// var reviewsHTMLel = document.getElementById('reviews');
+var templateNewReview = document.createElement('tr');
+var templateReview = document.createElement('tr');
+
+templateNewReview.dataset.review = "reviewNew";
+templateNewReview.dataset.review = "review";
+
+templateNewReview.innerHTML = `
+<td data-review="reviewText"> </td>
+<td> <button data-btn="reviewApprove">+</button></td>
+<td> <button data-btn="reviewDecline">-</button></td>`;
+
+templateReview.innerHTML = `
+<td data-review="reviewText"> </td>
+`;
+
+function Reviews(idReviews) {
+  this.idReviews = idReviews;
+}
+
+Reviews.prototype.create = function () {
+  var reviewsHTMLel = document.createElement('table');
+  reviewsHTMLel.id = this.idReviews;
+  return reviewsHTMLel;
+};
+
