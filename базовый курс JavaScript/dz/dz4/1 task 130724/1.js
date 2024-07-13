3649a0047fc723cdb0caa934d9880c1d46278076
SergeyShashin <s9997910@yandex.ru>
2024-07-13 21:05:04 +0300

    JS. Base. DZ4 task1 130724 done.

---
+++ базовый курс JavaScript/dz/dz4/1 task 130724/1.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/*
+Написать функцию, преобразующую число в объект. Передавая на вход число в диапазоне [0,
+999],
+мы должны получить на выходе объект, в котором в соответствующих свойствах описаны
+разряды числа:
+- единицы (в свойстве firstDigit)
+- десятки (в свойстве secondDigit)
+- сотни (в свойстве thirdDigit)
+Например, для числа 45 мы должны получить следующий объект:
+```
+{
+firstDigit: 5,
+secondDigit: 4,
+thirdDigit: 0,
+}
+```
+Если число было передано вне [0, 999] диапазона, не целое число или вообще не число,
+необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
+*/
+
+console.log(transformNumberToObj(805));
+
+function transformNumberToObj(number) {
+  return Number.isInteger(number)
+    ? {
+      firstDigit: parseInt(number % 10),
+      secondDigit: parseInt(number / 10 % 10),
+      thirdDigit: parseInt(number / 100),
+    }
+    : {}
+}
