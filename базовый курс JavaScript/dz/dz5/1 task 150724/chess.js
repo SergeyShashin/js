3f8bad923fc5fda3bcf87b487baf0b8879c6d6a1
SergeyShashin <s9997910@yandex.ru>
2024-07-15 17:18:52 +0300

    JS. Base. dz5 task1 150724 updated.

--- базовый курс JavaScript/dz/dz5/1 task 150724/chess.js
+++ базовый курс JavaScript/dz/dz5/1 task 150724/chess.js
@@ -7,14 +7,37 @@
 // Строки должны нумероваться числами от 1 до 8, столбцы – латинскими буквами A, B, C, D, E, F,
 // G, H.
 
+generateChessboard();
+
 function generateChessboard() {
   let HTMLeLForChess = document.getElementById('chess');
+  let letеers = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', ''];
 
   for (let row = 0; row < 10; row++) {
-    let tr = document.createElement(tr);
-    for (let col = 0; col < 10; col++) {
-    }
 
+    let tr = document.createElement('tr');
+    HTMLeLForChess.appendChild(tr);
+
+    for (let col = 0; col < 10; col++) {
+      let td = document.createElement('td');
+      tr.appendChild(td);
+
+      //вставляет цифры в 0 колонку
+      if (col === 0 && row > 0 && row < 9||col === 9 && row > 0 && row < 9) {
+        td.textContent = Math.abs(row-9);
+      }
+
+      //вставляет цифры в 9 колонку
+      if (col === 9 && row > 0 && row < 9) {
+        td.textContent = row;
+      }
+
+      //вставляет символу в 9 строку
+      if (row === 9 && col > 0 && col < 9) {
+        td.textContent = letеers[col];
+      }
+
+    }
   }
 
 
