85c5e483a155deda47a0dd1fd65411a1a3ba5d78
SergeyShashin <s9997910@yandex.ru>
2024-07-18 23:20:52 +0300

    JS. Base. Lesson6 180724. Gallery. Done.

--- базовый курс JavaScript/lessons/lesson6/gallery 180724/gallery.js
+++ базовый курс JavaScript/lessons/lesson6/gallery 180724/gallery.js
@@ -1,16 +1,67 @@
 'use strict';
 
 const gallery = {
+
   settings: {
     idHTMLElGallery: 'gallery',
-    nameClassWrap: '.gallery-wrap',
+    nameIdWrap: 'imgBtnWrap',
+    nameClassWrap: 'img-btn-wrap',
+    classFullImg: 'img-max',
     pathToImgBtnClose: 'img/gallery/close.png',
+    classBtnClose: 'btn-close',
   },
 
-  init(userSeting={}) {
-    console.log('Старт.');
+  elementHTMLGallery: null,
 
+  init(userSetings = {}) {
+    Object.assign(this.settings, userSetings);
+    this.elementHTMLGallery = document.getElementById(this.settings.idHTMLElGallery);
+    this.elementHTMLGallery.addEventListener('click', (e) => this.handlerClickGallery(e));
+  },
+
+  handlerClickGallery(e) {
+    if (e.target.tagName !== 'IMG') {
+      return
+    }
+    let srcFullImgUrl = e.target.dataset.fullImageUrl;
+    this.openImg(srcFullImgUrl);
+  },
+
+  openImg(src) {
+    let fullImg = this.getScreenContainer().querySelector('.' + this.settings.classFullImg);
+    fullImg.src = src;
+  },
+
+  getScreenContainer() {
+    let screenContainer = document.getElementById('imgBtnWrap');
+    return screenContainer ? screenContainer : this.createScreenContainer()
+  },
+
+  createScreenContainer() {
+    let container = document.createElement('div');
+    container.id = this.settings.nameIdWrap;
+    container.className = this.settings.nameClassWrap;
+    document.body.appendChild(container);
+
+    let fullImg = new Image();
+    fullImg.className = this.settings.classFullImg;
+    container.appendChild(fullImg);
+
+    let btnClose = document.createElement('button');
+    btnClose.className = this.settings.classBtnClose;
+    let imgBtnClose = new Image();
+    imgBtnClose.src = this.settings.pathToImgBtnClose;
+    btnClose.appendChild(imgBtnClose);
+    container.appendChild(btnClose);
+    btnClose.addEventListener('click', () => this.close());
+
+    return container
+  },
+
+  close() {
+    document.getElementById(this.settings.nameIdWrap).remove();
   }
+
 }
 
 window.onload = () => gallery.init()
