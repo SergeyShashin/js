29cc82f243db501a42d093e8cfb650e2697df1db
SergeyShashin <s9997910@yandex.ru>
2024-07-21 16:03:21 +0300

    JS. Base. DZ6 task3 210724 start.

---
+++ базовый курс JavaScript/dz/dz6/3 task 210724/gallery.js
@@ -0,0 +1,88 @@
+'use strict';
+
+/*
+3*. Добавить в галерею функцию перехода к следующему изображению. По сторонам от большой картинки
+должны быть стрелки “вперед” и “назад”, по нажатию на которые происходит замена изображения на
+следующее или предыдущее.
+*/
+
+const gallery = {
+
+  settings: {
+    idHTMLElGallery: 'gallery',
+    nameIdWrap: 'imgBtnWrap',
+    nameClassWrap: 'img-btn-wrap',
+    classFullImg: 'img-max',
+    pathToImgBtnClose: 'img/gallery/close.png',
+    pathToNothingImg: 'img/gallery/nothing.png',
+    classBtnClose: 'btn-close',
+  },
+
+  elementHTMLGallery: null,
+
+  init(userSetings = {}) {
+    Object.assign(this.settings, userSetings);
+    this.elementHTMLGallery = document.getElementById(this.settings.idHTMLElGallery);
+    this.elementHTMLGallery.addEventListener('click', (e) => this.handlerClickGallery(e));
+  },
+
+  handlerClickGallery(e) {
+    if (e.target.tagName !== 'IMG') {
+      return
+    }
+    let srcFullImgUrl = e.target.dataset.fullImageUrl;
+    // this.openImg(srcFullImgUrl);
+    let checkImg = new Image();
+    checkImg.onload = () => this.openImg(srcFullImgUrl);
+    checkImg.onerror = () => this.openImg(this.settings.pathToNothingImg);
+    checkImg.src = srcFullImgUrl;
+  },
+
+  openImg(src) {
+    let fullImg = this.getScreenContainer().querySelector('.' + this.settings.classFullImg);
+    fullImg.src = src;
+  },
+
+  getScreenContainer() {
+    let screenContainer = document.getElementById('imgBtnWrap');
+    return screenContainer ? screenContainer : this.createScreenContainer()
+  },
+
+  createScreenContainer() {
+    let container = document.createElement('div');
+    container.id = this.settings.nameIdWrap;
+    container.className = this.settings.nameClassWrap;
+    document.body.appendChild(container);
+
+    let fullImg = new Image();
+    fullImg.className = this.settings.classFullImg;
+    container.appendChild(fullImg);
+
+    let btnClose = document.createElement('button');
+    btnClose.className = this.settings.classBtnClose;
+    let imgBtnClose = new Image();
+    imgBtnClose.src = this.settings.pathToImgBtnClose;
+
+    let btnLeft = document.createElement('div');
+    btnLeft.textContent = '<';
+    btnLeft.className = 'btnLeft';
+    let btnRight = document.createElement('div');
+    btnRight.textContent = '>';
+    btnRight.className = 'btnRight';
+
+    btnClose.appendChild(imgBtnClose);
+    container.appendChild(btnClose);
+    container.appendChild(btnLeft);
+    container.appendChild(btnRight);
+    btnClose.addEventListener('click', () => this.close());
+
+    return container
+  },
+
+  close() {
+    document.getElementById(this.settings.nameIdWrap).remove();
+  }
+
+}
+
+window.onload = () => gallery.init()
