f0ea695d4e304a58be03e0a118b6a6b20c36f783
SergeyShashin <s9997910@yandex.ru>
2024-07-29 16:26:49 +0300

    JS. Advanced. DZ7 lesson1 290724 done.

---
+++ продвинутый курс JavaScript/lessons/lesson1/menu290724/menu290724.js
@@ -0,0 +1,55 @@
+function Container(someId, someClass) {
+  this.id = someId;
+  this.class = someClass;
+}
+
+Container.prototype.render = function () {
+  var div = document.createElement('div');
+  div.id = this.id;
+  div.className = this.class;
+
+  return div
+};
+
+function Menu(someId, someClass, items) {
+  Container.call(this, someId, someClass);
+  this.items = items;
+}
+
+Menu.prototype = Object.create(Container.prototype);
+
+Menu.prototype.render = function () {
+  var ul = document.createElement('ul');
+  ul.id = this.id;
+  ul.className = this.class;
+
+  this.items.map(function (item) {
+    if (item instanceof Container) {
+      ul.appendChild(item.render());
+    }
+  })
+
+  return ul
+};
+
+
+function MenuItem(href, label) {
+  Container.call(this, 'idMenuItem', 'classIdMenuItem');
+  this.href = href;
+  this.label = label;
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+MenuItem.prototype.render = function () {
+  var a = document.createElement('a');
+  var li = document.createElement('li');
+
+  a.href = this.href;
+  a.textContent = this.label;
+  li.id = this.id;
+  li.className = this.class;
+
+  li.appendChild(a);
+
+  return li
+}
