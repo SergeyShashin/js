3a615fe6cbec9e005772a1a9acc4414597faafbe
SergeyShashin <s9997910@yandex.ru>
2024-08-02 00:04:04 +0300

    JS. Advanced. DZ3 task3 010824 done.

--- продвинутый курс JavaScript/dz/dz3/task3 010824/validation.js
+++ продвинутый курс JavaScript/dz/dz3/task3 010824/validation.js
@@ -15,13 +15,51 @@
 */
 
 var rules = {
-  name: '/[a-za-яё]/ig',
-  phone: '/\+\d\(\d{3}\)\d{3}\-\d{4}/'
+  name: /[a-za-яё]/,
+  phone: /\+\d\(\d{3}\)\d{3}\-\d{4}/,
+  email:/[\w/./-]{1,}\@[\w/./-]{1,}\.[\w/./-]{1,3}/
 };
 
-var data = document.getElementsByTagName('input');
-
-for (var item of data) {
-  console.log(item);
+var errors = {
+  name: 'Имя может содержать только буквы',
+  phone: 'Телефон подчиняется шаблону +7(000)000-0000',
+  email:'E-mail выглядит как mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru'
 }
+
+
+var formContacts = document.getElementById('formContacts');
+
+formContacts.addEventListener('submit', function (e) {
+  e.preventDefault();
+var data = document.getElementsByTagName('input');
+var isValid=true;
+
+ 
+ for (var item of data) {
+  var rule = rules[item.id];
+  var error = errors[item.id];
+
+  var content = item.value;
+  var rgxp = new RegExp(rule);
+
+  var resultValidation = rgxp.test(content);
+  if(!resultValidation){
+    item.style.background='red';
+    alert(error);
+    isValid=false;
+    break;
+  }else{
+    item.style.background='none';
+    isValid=true;
+  }
+}
+
+if(isValid){
+  alert('Отправлено');
+  document.body.append(formContacts);
+  formContacts.submit();
+}
+});
+
+
 
