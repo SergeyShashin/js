6ea4234996c2ab64df3f3ecbd0866715c84f7548
SergeyShashin <s9997910@yandex.ru>
2024-08-06 16:14:29 +0300

    JS. Advanced. Lesson5 060824 updated.

--- продвинутый курс JavaScript/lessons/lesson5/060824/cart.js
+++ продвинутый курс JavaScript/lessons/lesson5/060824/cart.js
@@ -4,6 +4,33 @@ var cartEl = document.getElementById('cart');
 buildList();
 buildCart();
 
+document.getElementById('pageCatalog').addEventListener('click', function (e) {
+  var target = e.target;
+  switch (target.id) {
+    case "btnBuy":
+      console.log(target.dataset.id);
+
+      dataProduct = {
+        id: target.dataset.id,
+        name: target.dataset.name,
+        price: target.dataset.price,
+        quantity: 1
+      };
+
+      console.log(dataProduct);
+
+      sendData('POST', 'http://localhost:3000/cart', dataProduct);
+      // sendData('PATCH', 'http://localhost:3000/cart', dataProduct);
+      break;
+    case "btnDel":
+      console.log(target.dataset.id);
+      break;
+  }
+
+});
+
+
+
 function buildList() {
   getDataFromJson("GET", "http://localhost:3000/goods", function (data) {
     var goodsEl = document.getElementById('goods');
@@ -15,9 +42,11 @@ function buildList() {
       var tdBtnBuy = document.createElement('button');
       tdNameEl.textContent = product.name;
       tdPriceEl.textContent = product.price;
+      tdBtnBuy.id = 'btnBuy';
       tdBtnBuy.textContent = 'купить';
       tdBtnBuy.dataset.id = product.id;
-
+      tdBtnBuy.dataset.name = product.name;
+      tdBtnBuy.dataset.price = product.name;
       tr.appendChild(tdNameEl);
       tr.appendChild(tdPriceEl);
       tr.appendChild(tdBtnBuy);
@@ -28,6 +57,7 @@ function buildList() {
   })
 
 }
+
 function buildCart() {
   getDataFromJson("GET", "http://localhost:3000/cart", function (data) {
     var goodsEl = document.getElementById('cart');
@@ -37,16 +67,20 @@ function buildCart() {
       var tdNameEl = document.createElement('td');
       var tdPriceEl = document.createElement('td');
       var tdQuantityEl = document.createElement('td');
+      var tdSquEl = document.createElement('td');
       var tdBtnDel = document.createElement('button');
       tdNameEl.textContent = product.name;
       tdPriceEl.textContent = product.price;
       tdQuantityEl.textContent = product.quantity;
+      tdBtnDel.id = 'btnDel';
+      tdSquEl.textContent = 'шт.';
       tdBtnDel.textContent = 'x';
       tdBtnDel.dataset.id = product.id;
 
       tr.appendChild(tdNameEl);
       tr.appendChild(tdPriceEl);
       tr.appendChild(tdQuantityEl);
+      tr.appendChild(tdSquEl);
       tr.appendChild(tdBtnDel);
 
       goodsEl.appendChild(tr);
@@ -68,5 +102,9 @@ function getDataFromJson(method, link, callback) {
     }
   }
 
+
+}
+
+function sendData(method, link, data) {
 
 }
