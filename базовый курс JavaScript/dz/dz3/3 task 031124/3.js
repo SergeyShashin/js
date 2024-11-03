14616acc6747488044f3dcdf31dd14f89acaa5fa
SergeyShashin <s9997910@yandex.ru>
2024-11-03 15:06:11 +0300

    JS. Base. DZ3 3task 031124.

---
+++ базовый курс JavaScript/dz/dz3/3 task 031124/3.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+Нарисовать горку с помощью console.log (используя цикл for), как показано на
+рисунке,
+только у вашей горки должно быть 20 рядов, а не 5:
+```
+x
+xxx
+xxxxx
+xxxxxxx
+xxxxxxxxx
+*/
+
+for (let i = 0, x = 'x'; i < 20; i++ , console.log(x), x += 'xx') { }
