f34b305f40ba95010b6ff6b2d6616a1065eb5cae
SergeyShashin <s9997910@yandex.ru>
2024-12-06 16:02:22 +0300

    JS. Advanced. DZ1. Task3 061224. Burger updated.

--- продвинутый курс JavaScript/dz/dz1/task3 061224/burger.js
+++ продвинутый курс JavaScript/dz/dz1/task3 061224/burger.js
@@ -14,6 +14,8 @@
 */
 
 function Burger(size, stuffing) {
+  this.size;
+  this.stuffing;
   this.price = 0;
   this.calories = 0;
   this.toppings = [];
@@ -22,10 +24,12 @@ function Burger(size, stuffing) {
     case BURGER_SIZE_BIG:
       this.price += 100;
       this.calories += 40;
+      this.size = 'большой';
       break;
     case BURGER_SIZE_SMALL:
       this.price += 50;
       this.calories += 20;
+      this.size = 'маленький';
       break;
   }
 
@@ -33,14 +37,17 @@ function Burger(size, stuffing) {
     case BURGER_STUFFINGS_CHEESE:
       this.price += 10;
       this.calories += 20;
+      this.stuffing = 'сыр';
       break;
     case BURGER_STUFFINGS_SALAD:
       this.price += 20;
       this.calories += 5;
+      this.stuffing = 'салат';
       break;
     case BURGER_STUFFINGS_POTATO:
       this.price += 15;
       this.calories += 10;
+      this.stuffing = 'картофель';
       break;
   }
 
@@ -67,7 +74,23 @@ Burger.prototype.addTopping = function (topping) {
         break;
     }
   }
-}
+};
+
+Burger.prototype.getSize = function () {
+  return this.size;
+};
+
+Burger.prototype.getStuffing = function () {
+  return this.stuffing;
+};
+
+Burger.prototype.getToppings = function () {
+  let result = [];
+  for (let topping of this.toppings) {
+    topping === BURGER_TOPPINGS_SPICE ? result.push('приправа') : result.push('майонез');
+  }
+  return result;
+};
 
 var BURGER_SIZE_BIG = 1;
 var BURGER_SIZE_SMALL = 2;
@@ -76,3 +99,4 @@ var BURGER_STUFFINGS_SALAD = 2;
 var BURGER_STUFFINGS_POTATO = 3;
 var BURGER_TOPPINGS_SPICE = 1;
 var BURGER_TOPPINGS_MAYO = 2;
+
