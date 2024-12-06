dbf8cf9d97a9ec3439ead30bb23c44e62e70d999
SergeyShashin <s9997910@yandex.ru>
2024-12-06 15:21:28 +0300

    JS. Advanced. DZ1. Task3 061224. Burger updated.

--- продвинутый курс JavaScript/dz/dz1/task3 061224/burger.js
+++ продвинутый курс JavaScript/dz/dz1/task3 061224/burger.js
@@ -13,3 +13,66 @@
 (подсказка: нужен класс Гамбургер, константы, методы для выбора опций и расчета нужных величин).
 */
 
+function Burger(size, stuffing) {
+  this.price = 0;
+  this.calories = 0;
+  this.toppings = [];
+
+  switch (size) {
+    case BURGER_SIZE_BIG:
+      this.price += 100;
+      this.calories += 40;
+      break;
+    case BURGER_SIZE_SMALL:
+      this.price += 50;
+      this.calories += 20;
+      break;
+  }
+
+  switch (stuffing) {
+    case BURGER_STUFFINGS_CHEESE:
+      this.price += 10;
+      this.calories += 20;
+      break;
+    case BURGER_STUFFINGS_SALAD:
+      this.price += 20;
+      this.calories += 5;
+      break;
+    case BURGER_STUFFINGS_POTATO:
+      this.price += 15;
+      this.calories += 10;
+      break;
+  }
+
+}
+
+Burger.prototype.getPrice = function () {
+  return this.price;
+};
+
+Burger.prototype.getCalories = function () {
+  return this.calories;
+};
+
+Burger.prototype.addTopping = function (topping) {
+  if (!this.toppings.includes(topping)) {
+    this.toppings.push(topping);
+    switch (topping) {
+      case BURGER_TOPPINGS_SPICE:
+        this.price += 15;
+        break;
+      case BURGER_TOPPINGS_MAYO:
+        this.price += 20;
+        this.calories += 5;
+        break;
+    }
+  }
+}
+
+var BURGER_SIZE_BIG = 1;
+var BURGER_SIZE_SMALL = 2;
+var BURGER_STUFFINGS_CHEESE = 1;
+var BURGER_STUFFINGS_SALAD = 2;
+var BURGER_STUFFINGS_POTATO = 3;
+var BURGER_TOPPINGS_SPICE = 1;
+var BURGER_TOPPINGS_MAYO = 2;
