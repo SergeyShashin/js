eccd5e9493092d0bcbce5b7e60015ff821243d3c
SergeyShashin <s9997910@yandex.ru>
2025-01-01 00:58:54 +0300

    JS advanced. Lesson5 311224 updated.

--- продвинутый курс JavaScript/lessons/lesson5/311224/cart.js
+++ продвинутый курс JavaScript/lessons/lesson5/311224/cart.js
@@ -23,6 +23,10 @@
           var thPriceEl = document.createElement('th');
           var thForBtnOrderEl = document.createElement('th');
 
+          thIdEl.textContent = 'id';
+          thNameEl.textContent = 'name';
+          thPriceEl.textContent = 'price';
+
           tHeadEl.appendChild(thIdEl);
           tHeadEl.appendChild(thNameEl);
           tHeadEl.appendChild(thPriceEl);
@@ -38,10 +42,6 @@
             var tdPriceEl = document.createElement('td');
             var tdForBtnOrderEl = document.createElement('td');
             var btnOrderEl = document.createElement('button');
-
-            thIdEl.textContent = 'id';
-            thNameEl.textContent = 'name';
-            thPriceEl.textContent = 'price';
 
             tdIdEl.textContent = el.id;
             tdNameEl.textContent = el.name;
@@ -69,7 +69,76 @@
         url: 'http://localhost:3000/goodsIncart',
         dataType: 'json',
         success(data) {
-          console.log(data);
+          var goodsInCartEl = $('#goodsInCart')[0];
+          var totalSum = 0;
+          goodsInCartEl.innerHTML = '';
+
+          var tHeadEl = document.createElement('thead');
+          var tBodyEl = document.createElement('tbody');
+          var thIdEl = document.createElement('th');
+          var thNameEl = document.createElement('th');
+          var thPriceEl = document.createElement('th');
+          var thQuantityEl = document.createElement('th');
+          var thSumEl = document.createElement('th');
+          var thForBtnDeleteEl = document.createElement('th');
+
+          thIdEl.textContent = 'id';
+          thNameEl.textContent = 'name';
+          thPriceEl.textContent = 'price';
+          thQuantityEl.textContent = 'quantity';
+          thSumEl.textContent = 'sum';
+
+          tHeadEl.appendChild(thIdEl);
+          tHeadEl.appendChild(thNameEl);
+          tHeadEl.appendChild(thPriceEl);
+          tHeadEl.appendChild(thQuantityEl);
+          tHeadEl.appendChild(thSumEl);
+          tHeadEl.appendChild(thForBtnDeleteEl);
+
+          for (let el of data) {
+            var trGoodEl = document.createElement('tr');
+            var tdIdEl = document.createElement('td');
+            var tdNameEl = document.createElement('td');
+            var tdPriceEl = document.createElement('td');
+            var tdQuantityEl = document.createElement('td');
+            var tdSumEl = document.createElement('td');
+            var tdForBtnDeleteEl = document.createElement('td');
+            var btnDeleteEl = document.createElement('button');
+
+            tdIdEl.textContent = el.id;
+            tdNameEl.textContent = el.name;
+            tdPriceEl.textContent = el.price;
+            tdQuantityEl.textContent = el.quantity;
+            tdSumEl.textContent = Number(el.quantity) * Number(el.price);
+
+            totalSum += Number(tdSumEl.textContent);
+
+            btnDeleteEl.textContent = 'x';
+            btnDeleteEl.dataset.id = el.id;
+            btnDeleteEl.dataset.name = el.name;
+            btnDeleteEl.dataset.price = el.price;
+            btnDeleteEl.dataset.quantity = el.quantity;
+
+            tdForBtnDeleteEl.appendChild(btnDeleteEl);
+
+            trGoodEl.appendChild(tdIdEl);
+            trGoodEl.appendChild(tdNameEl);
+            trGoodEl.appendChild(tdPriceEl);
+            trGoodEl.appendChild(tdQuantityEl);
+            trGoodEl.appendChild(tdSumEl);
+            trGoodEl.appendChild(tdForBtnDeleteEl);
+            tBodyEl.appendChild(trGoodEl);
+          }
+
+          var trEl = document.createElement('tr');
+          var tdTotalSumEl = document.createElement('td');
+          tdTotalSumEl.textContent = totalSum;
+          trEl.appendChild(tdTotalSumEl);
+          tBodyEl.appendChild(trEl);
+
+          goodsInCartEl.appendChild(tHeadEl);
+          goodsInCartEl.appendChild(tBodyEl);
+
         }
       });
 
@@ -88,13 +157,20 @@
 
         function addGoodInCart(goodEl) {
           var goodId = goodEl.dataset.id;
+          var goodInCartEl = $(`#goodsInCart [data-id="${goodId}"]`)[0];
 
-          if ($(`#goodsInCart [data-id="${goodId}"]`)[0]) {
+          if (goodInCartEl) {
             $.ajax({
               url: `http://localhost:3000/goodsIncart/${goodId}`,
               method: 'PATCH',
+              headers: {
+                'content-type': 'application/json'
+              },
+              data: JSON.stringify({
+                quantity: Number(goodInCartEl.dataset.quantity) + 1,
+              }),
               success() {
-                buildCart();
+                cart.buildCart();
               }
             });
           } else {
