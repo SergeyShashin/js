60090c9c8ec5bec3cc1d4e3f1addbbc9792105e2
SergeyShashin <s9997910@yandex.ru>
2024-12-31 19:57:05 +0300

    JS Advanced. Lesson5 311224 updated.

--- продвинутый курс JavaScript/lessons/lesson5/311224/cart.js
+++ продвинутый курс JavaScript/lessons/lesson5/311224/cart.js
@@ -1,26 +1,82 @@
-var cart = {
-  goodsEl: null,
-  goodsInCartEl: null,
-  goods: null,
-  goodsInCart: null,
+(function ($) {
+  var cart = {
+    goodsInCartEl: null,
 
-  init() {
-    this.goodsEl = document.getElementById('goods');
-    this.goodsInCartEl = document.getElementById('goodsInCartEl');
-    this.goods = [];
-    this.goodsInCart = [];
-    this.buildGoodsList();
-    this.buildCart();
-  },
+    init() {
+      this.goodsInCartEl = $('#goodsInCartEl');
+      this.buildGoodsList();
+      this.buildCart();
+    },
 
-  buildGoodsList() {
+    buildGoodsList() {
+      $.ajax({
+        url: 'http://localhost:3000/goods',
+        dataType: 'json',
+        success(data) {
+          var goodsEl = $('#goods')[0];
 
-  },
+          var tHeadEl = document.createElement('thead');
+          var tBodyEl = document.createElement('tbody');
+          var thIdEl = document.createElement('th');
+          var thNameEl = document.createElement('th');
+          var thPriceEl = document.createElement('th');
+          var thForBtnOrderEl = document.createElement('th');
 
-  buildCart() {
+          tHeadEl.appendChild(thIdEl);
+          tHeadEl.appendChild(thNameEl);
+          tHeadEl.appendChild(thPriceEl);
+          tHeadEl.appendChild(thForBtnOrderEl);
 
-  },
+          goodsEl.appendChild(tHeadEl);
+          goodsEl.appendChild(tBodyEl);
 
-}
+          for (let el of data) {
+            var trGoodEl = document.createElement('tr');
+            var tdIdEl = document.createElement('td');
+            var tdNameEl = document.createElement('td');
+            var tdPriceEl = document.createElement('td');
+            var tdForBtnOrderEl = document.createElement('td');
+            var btnOrderEl = document.createElement('button');
 
-window.onload = cart.init()
+            thIdEl.textContent = 'id';
+            thNameEl.textContent = 'name';
+            thPriceEl.textContent = 'price';
+
+            tdIdEl.textContent = el.id;
+            tdNameEl.textContent = el.name;
+            tdPriceEl.textContent = el.price;
+
+            btnOrderEl.textContent = 'Заказать';
+            btnOrderEl.dataset.id = el.id;
+            btnOrderEl.dataset.name = el.name;
+            btnOrderEl.dataset.price = el.price;
+
+            tdForBtnOrderEl.appendChild(btnOrderEl);
+
+            trGoodEl.appendChild(tdIdEl);
+            trGoodEl.appendChild(tdNameEl);
+            trGoodEl.appendChild(tdPriceEl);
+            trGoodEl.appendChild(tdForBtnOrderEl);
+            tBodyEl.appendChild(trGoodEl);
+          }
+        }
+      });
+    },
+
+    buildCart() {
+      $.ajax({
+        url: 'http://localhost:3000/goodsIncart',
+        dataType: 'json',
+        success(data) {
+          console.log(data);
+        }
+      });
+
+    },
+
+  };
+
+  window.onload = cart.init();
+})(jQuery);
+
+
