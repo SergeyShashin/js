95487a1327cec07b35ed319a4775fdd88de4ab8d
SergeyShashin <s9997910@yandex.ru>
2025-01-04 20:41:23 +0300

    JS Advanced. DZ7 task1 040125 done.

--- продвинутый курс JavaScript/dz/dz7/task1 040125/gulpfile.js
+++ продвинутый курс JavaScript/dz/dz7/task1 040125/gulpfile.js
@@ -8,20 +8,28 @@ var browserSync = require('browser-sync');
 //     pipe(gulp.dest('src/css/'));
 // });
 
-gulp.task('browserSync', function () {
-  browserSync({
-    server: { baseDir: 'src' }
-  })
-});
+// gulp.task('browserSync', function () {
+//   browserSync({
+//     server: { baseDir: 'src' }
+//   })
+// });
 
 gulp.task('watch', function () {
   var { watch } = require('gulp');
 
-  var watcher = watch(['src/scss/**/*.scss']);
+  var watcher = watch(['src/scss/**/*.scss', 'src/*.html']);
+
+  browserSync({
+    server: { baseDir: 'src' }
+  });
 
   watcher.on('change', function () {
-    gulp.src('src/scss/**/*.scss').
-      pipe(scss()).
-      pipe(gulp.dest('src/css/'));
+    gulp.src('src/scss/**/*.scss')
+    .pipe(scss())
+    .pipe(gulp.dest('src/css/'))
+    .pipe(browserSync.reload({ stream: true }));
+    
+    gulp.src('src/*.html')
+    .pipe(browserSync.reload({ stream: true }));
   });
 });
