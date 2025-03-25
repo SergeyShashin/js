4725a9f79c1d3abedec0f8a7ab84cd6b01d21a92
SergeyShashin <s9997910@yandex.ru>
2025-03-25 13:45:46 +0300

    JS Base. Lesson4 250325 done.

--- базовый курс JavaScript/lessons/lesson4/walker 250325.js
+++ базовый курс JavaScript/lessons/lesson4/walker 250325.js
@@ -14,15 +14,72 @@ const player = {
   init(startPositionX, startPositionY) {
     this.x = startPositionX;
     this.y = startPositionY;
-  }
+  },
 
-}
+  move(direction) {
+    switch (direction) {
+      case '4':
+        this.x--
+        break;
+      case '6':
+        this.x++
+        break;
+      case '8':
+        this.y--
+        break;
+      case '5':
+        this.y++
+        break;
+    }
+
+  }
+};
 
 const game = {
   settings,
   player,
+
   run() {
     console.log('Welcome World!)');
+
+    this.player.init(this.settings.startPositionX, this.settings.startPositionY);
+
+    while (true) {
+      this.render();
+      let direction = this.getDirection();
+
+      if (direction === '-1') {
+        alert('До встречи!)');
+        return
+      }
+
+      this.player.move(direction);
+    }
+  },
+
+  render() {
+    console.clear();
+    let map = '';
+    for (let row = 0; row < this.settings.rowsCount; row++) {
+      for (let col = 0; col < this.settings.colsCount; col++) {
+
+        col === this.player.x && row === this.player.y ? map += ':) ' : map += 'X  ';
+      }
+      map += '\n\n';
+    }
+    console.log(map);
+  },
+
+  getDirection() {
+    let availableDirections = ['4', '5', '6', '8', '-1'];
+
+    while (true) {
+      let direction = prompt(`Возможные направления: ${availableDirections}. Для выхода -1.`);
+
+      if (availableDirections.includes(direction)) {
+        return direction
+      }
+    }
   }
 };
 
