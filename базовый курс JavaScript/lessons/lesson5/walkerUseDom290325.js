46a31c78e74fdcfc87579d3e8c1eb1e55ffb4e70
SergeyShashin <s9997910@yandex.ru>
2025-03-29 19:50:36 +0300

    JS Base. Lesson5 walkerUseDom290325 done.

--- базовый курс JavaScript/lessons/lesson5/walkerUseDom290325.js
+++ базовый курс JavaScript/lessons/lesson5/walkerUseDom290325.js
@@ -24,7 +24,41 @@ const player = {
 
   getPostionCol() {
     return this.col
+  },
+
+  getNextPoint() {
+    let point = {
+      row: this.row,
+      col: this.col
+    }
+
+    switch (this.direction) {
+      case 'up':
+        point.row--;
+        break;
+      case 'down':
+        point.row++;
+        break;
+      case 'left':
+        point.col--;
+        break;
+      case 'right':
+        point.col++;
+        break;
+    }
+
+    return point;
+  },
+
+  move(nextPointPlayer) {
+    this.row = nextPointPlayer.row;
+    this.col = nextPointPlayer.col;
+  },
+
+  setDirection(direction) {
+    this.direction = direction;
   }
+
 };
 
 const map = {
@@ -57,10 +91,9 @@ const map = {
 
   render(playerPositionRow, playerPositionCol) {
     this.playerCell.classList.remove('player');
-    this.playerCell = this.cells[`${playerPositionRow}, ${playerPositionCol}`];
+    this.playerCell = this.cells[`${playerPositionRow}_${playerPositionCol}`];
     this.playerCell.classList.add('player');
   }
-
 
 };
 
@@ -71,14 +104,55 @@ const game = {
 
   run() {
     this.init();
+    this.move();
+    this.setEventHandlers();
   },
 
   init() {
     this.player.init(this.settings.startPostionRow, this.settings.startPostionCol, 'up');
     this.map.init(this.settings.rowsCount, this.settings.colsCount, this.player.getPostionRow(), this.player.getPostionCol());
-    // this.map.render(this.player.getPostionRow(), this.player.getPostionCol());
+  },
+
+  move() {
+    setInterval(() => {
+      let nextPointPlayer = this.player.getNextPoint();
+      if (this.isCanMove(nextPointPlayer)) {
+        this.player.move(nextPointPlayer);
+        this.map.render(nextPointPlayer.row, nextPointPlayer.col);
+      }
+    },
+      1000 / this.settings.speed);
+  },
+
+  isCanMove(nextPointPlayer) {
+    return nextPointPlayer.row >= 0 &&
+      nextPointPlayer.col >= 0 &&
+      nextPointPlayer.row < this.settings.rowsCount &&
+      nextPointPlayer.col < this.settings.colsCount
+  },
+
+  setEventHandlers() {
+    window.document.addEventListener('keydown', (e) => this.handlerKewDown(e));
+  },
+
+  handlerKewDown(e) {
+    switch (e.key) {
+      case 'ArrowUp':
+        this.player.setDirection('up');
+        break;
+      case 'ArrowDown':
+        this.player.setDirection('down');
+        break;
+      case 'ArrowLeft':
+        this.player.setDirection('left');
+        break;
+      case 'ArrowRight':
+        this.player.setDirection('right');
+        break;
+
+    }
   }
 
 };
 
-game.run();
+window.onload = game.run();
