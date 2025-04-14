6e7ee94a06c71c32e7b890fa932fe58219e4db0b
SergeyShashin <s9997910@yandex.ru>
2025-04-14 17:02:25 +0300

    JS Advanced. DZ2 task3 140425 updated.

--- продвинутый курс JavaScript/dz/dz1/task3 140425/burger.js
+++ продвинутый курс JavaScript/dz/dz1/task3 140425/burger.js
@@ -14,3 +14,66 @@
 (подсказка: нужен класс Гамбургер, константы, методы для выбора опций и расчета нужных величин).
 */
 
+function Burger(size, stuffing) {
+  this.size = size;
+  this.stuffing = stuffing;
+  this.spices = [];
+  this.price = 0;
+  this.calories = 0;
+
+  switch (this.size) {
+    case 'big':
+      this.price += 100;
+      this.calories += 40;
+      break;
+    case 'small':
+      this.price += 50;
+      this.calories += 20;
+      break;
+    default: throw new BurgerError('Что-то не так с размером.');
+  }
+
+  switch (this.stuffing) {
+    case 'cheese':
+      this.price += 10;
+      this.calories += 20;
+      break;
+    case 'salad':
+      this.price += 20;
+      this.calories += 5;
+      break;
+    case 'potato':
+      this.price += 15;
+      this.calories += 10;
+      break;
+    default: throw new BurgerError('Что-то не так с начинкой.');
+  }
+}
+
+Burger.prototype.addSpice = function (spice) {
+  if (!this.spices.includes(spice)) {
+    this.spices.push(spice);
+    switch (spice) {
+      case 'sauce':
+        this.price += 15;
+        this.calories += 0;
+        break;
+      case 'mayo':
+        this.price += 20;
+        this.calories += 5;
+        break;
+    }
+  }
+};
+
+Burger.prototype.getPrice = function () {
+  return this.price;
+};
+
+Burger.prototype.getCalories = function () {
+  return this.calories;
+}
+
+function BurgerError(msg) {
+  return msg
+}
