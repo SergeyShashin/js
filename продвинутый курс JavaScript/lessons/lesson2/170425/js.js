f9124d5367d2659d9f1135a19eaf95c4cbe612e4
SergeyShashin <s9997910@yandex.ru>
2025-04-17 14:32:31 +0300

    JS Advanced. Lesson2 updated.

--- продвинутый курс JavaScript/lessons/lesson2/170425/js.js
+++ продвинутый курс JavaScript/lessons/lesson2/170425/js.js
@@ -5,5 +5,31 @@ btnBuildMenuEl.addEventListener('click', function () {
 });
 
 function handlerClickBtnBuildMenu() {
-  console.log('Построй меню, пожалуйста.');  
+
+  function getData(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:3000/menu');
+    xhr.send();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        callback(data);
+      }
+    };
+  }
+
+  getData(function buldMenu(data) {
+    var itemsEls = [];
+    data.map(function (el) {
+      console.log(el.label);
+      console.log(el.path);
+      itemsEls.push(new MenuItem(el.label, el.path));
+    });
+    var monitorEl = document.getElementById('monitor');
+    console.log(monitorEl);
+    console.log(itemsEls);
+    var menu = new Menu('menu', 'menu', [itemsEls]);
+    monitorEl.appendChild(menu.render());
+  })
+
 }
