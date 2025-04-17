8f16670ef5988bf3560d2c58ca848e076038f3fc
SergeyShashin <s9997910@yandex.ru>
2025-04-17 12:22:53 +0300

    JS Lesson2. Advanced start.

---
+++ продвинутый курс JavaScript/lessons/lesson2/170425/js.js
@@ -0,0 +1,9 @@
+let btnBuildMenuEl = document.getElementById('btnBuildMenu');
+
+btnBuildMenuEl.addEventListener('click', function () {
+  handlerClickBtnBuildMenu()
+});
+
+function handlerClickBtnBuildMenu() {
+  console.log('Построй меню, пожалуйста.');
+}
