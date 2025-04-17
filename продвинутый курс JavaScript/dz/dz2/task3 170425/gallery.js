063229476f1a9ee427cad37dfcd93891b9d5dc9d
SergeyShashin <s9997910@yandex.ru>
2025-04-17 15:24:43 +0300

    JS Advanced. DZ2 task3 170425 done.

---
+++ продвинутый курс JavaScript/dz/dz2/task3 170425/gallery.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function getData(method, link, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, link);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  }
+}
+
+getData('GET', 'http://localhost:3000/images', builGallery);
+
+function builGallery(data) {
+  var galleryMiniEl = document.getElementById('galleryMini');
+  galleryMiniEl.addEventListener('click', function (e) {
+    if (e.target.tagName === 'IMG') {
+      var monitorEl = document.getElementById('monitor');
+      monitorEl.innerHTML = '';
+      var imgEl = new Image();
+      imgEl.src = e.target.dataset.imgMaxPath;
+      monitorEl.appendChild(imgEl);
+    }
+  });
+
+  for (let item of data) {
+    var imgEl = document.createElement('img');
+    var liEl = document.createElement('li');
+    imgEl.src = item.imgMiniPath;
+    imgEl.dataset.imgMaxPath = item.imgMaxPath;
+    liEl.appendChild(imgEl);
+    galleryMiniEl.appendChild(liEl);
+  }
+}
