a608502863c558839773f97846727a9ec25f62bf
SergeyShashin <s9997910@yandex.ru>
2025-07-15 12:44:16 +0300

    JS. Base. DZ 3 task2 150725 done.

---
+++ базовый курс JavaScript/dz/dz3/5 task 150725/simpleNumber.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*
+ С помощью цикла while вывести все простые числа в промежутке от 0 до 100
+(можно без оптимизаций).
+*/
+
+let i = 3;
+let simpleNumbers = [1, 2];
+
+while (i < 100) {
+  isNumberPrime(i) ? simpleNumbers.push(i) : '';
+  i++;
+}
+
+console.log(simpleNumbers);
+
+function isNumberPrime(number) {
+  for (let i = number - 1; i > 1; i--) {
+    if (number % i === 0) {
+      return false
+    }
+  }
+
+  return true
+}
