56c5ccce9b0642ac5d626fbf4972f7a47d461025
SergeyShashin <s9997910@yandex.ru>
2025-08-04 13:28:19 +0300

    JS. Base. Lesson6 040825 updated.

--- базовый курс JavaScript/lessons/lesson6/gallery 040825/gallery.js
+++ базовый курс JavaScript/lessons/lesson6/gallery 040825/gallery.js
@@ -9,7 +9,24 @@ const gallery = {
     Object.assign(this.settings, userSettings);
     this.galleryEl = document.getElementById('gallery');
     this.setEventHandlers();
-  }
+  },
+  setEventHandlers() {
+    this.galleryEl.addEventListener('click', e => this.handleClickOnGalleryEl(e))
+  },
+  handleClickOnGalleryEl(e) {
+    let eTarget = e.target;
+    console.log(eTarget);
+    if (eTarget.tagName === 'IMG') {
+      this.openImg(eTarget.dataset.fullImageUrl);
+    }
+  },
+  openImg(srcIMG) {
+    console.log(srcIMG);
+    let fullIMGEl = new Image();
+    fullIMGEl.src = srcIMG;
+
+  },
+
 
 }
 
