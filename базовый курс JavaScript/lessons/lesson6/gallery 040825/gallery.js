e8a2c3ad2a77014bb451f6b6e329e9d2de6cd6ee
SergeyShashin <s9997910@yandex.ru>
2025-08-04 13:57:40 +0300

    JS. Base. Lesson6 040825 updated.

--- базовый курс JavaScript/lessons/lesson6/gallery 040825/gallery.js
+++ базовый курс JavaScript/lessons/lesson6/gallery 040825/gallery.js
@@ -24,9 +24,30 @@ const gallery = {
     console.log(srcIMG);
     let fullIMGEl = new Image();
     fullIMGEl.src = srcIMG;
-
+    let containerElForFullIMG = this.getContainerElForFullIMG();
+    containerElForFullIMG.appendChild(fullIMGEl);
   },
+  getContainerElForFullIMG() {
+    let containerEl = document.querySelector('.monitor');
 
+    if (!containerEl) {
+      containerEl = this.createContainerEl();
+    }
+
+    return containerEl;
+  },
+  createContainerEl() {
+    let containerEl = document.createElement('div');
+    containerEl.classList.add('monitor');
+    let imgClose = new Image();
+    imgClose.src = this.settings.buttonCLosePathIMG;
+    containerEl.appendChild(imgClose);
+    let backGroundEl = document.createElement('div');
+    backGroundEl.classList.add('background');
+    containerEl.appendChild(backGroundEl);
+    document.body.appendChild(containerEl);
+    return containerEl
+  }
 
 }
 
