5bd96e2628eef0170885add65b7e011d22834883
SergeyShashin <s9997910@yandex.ru>
2025-08-05 20:50:58 +0300

    JS. Lesson6. TikTakToe 040825 done.

--- базовый курс JavaScript/lessons/lesson6/tikTakToe 040825/tikTakToe.js
+++ базовый курс JavaScript/lessons/lesson6/tikTakToe 040825/tikTakToe.js
@@ -28,6 +28,9 @@ const map = {
   },
   tdIsEmpty({ row, col }) {
     return this.mapArr[row][col] === '';
+  },
+  getMapArr() {
+    return this.mapArr
   }
 
 };
@@ -36,8 +39,18 @@ const tikTakToe = {
   map,
   phase: null,
   status: null,
+  smile1: null,
+  smile2: null,
+  line1: null,
+  line2: null,
+  counterCLick: null,
   init() {
-    this.phase = '(^_^)';
+    this.smile1 = '(^_^)';
+    this.smile2 = '(*_*)';
+    this.line1 = '(^_^)(^_^)(^_^)';
+    this.line2 = '(*_*)(*_*)(*_*)';
+    this.phase = this.smile1;
+    this.counterCLick = 0;
     this.map.init();
     this.setEventHandlers();
     this.status = 'play';
@@ -50,15 +63,54 @@ const tikTakToe = {
     if (eTarget.tagName !== 'TD' || !this.map.tdIsEmpty(eTarget.dataset) || !this.statusPlay()) {
       return
     }
+    this.counterCLick++;
     eTarget.textContent = this.phase;
     this.map.setCoordinates(eTarget.dataset, this.phase);
+
+    if (this.counterCLick > 3) {
+      this.check();
+    }
+
+    if (this.counterCLick === 9) {
+      this.status = 'stop';
+      return alert('3 одинаковых смайлика не собрали.');
+    }
+
     this.togglePhase();
   },
   togglePhase() {
-    this.phase = this.phase === '(^_^)' ? '(*_*)' : '(^_^)';
+    this.phase = this.phase === this.smile1 ? this.smile2 : this.smile1;
   },
   statusPlay() {
     return this.status === 'play';
+  },
+  check() {
+    let mapArr = this.map.getMapArr();
+
+    for (let i = 0; i < 3; i++) {
+      let arrLine = mapArr[i].join('');
+      let arrCol = '';
+      let arrDiagonal1 = '';
+      let arrDiagonal2 = '';
+
+      for (let j = 0, z = 2; j < 3; j++ , z--) {
+        arrCol += mapArr[j][i];
+        arrDiagonal1 += mapArr[j][j];
+        arrDiagonal2 += mapArr[j][z];
+      }
+
+      if (this.checkLine([arrLine, arrCol, arrDiagonal1, arrDiagonal2])) {
+        this.status = 'stop';
+        return alert(`${this.phase} собрали 3 смайлика подряд.`);
+      };
+    }
+  },
+  checkLine(lines) {
+    for (let line of lines) {
+      if (line === this.line1 || line == this.line2) {
+        return true
+      }
+    }
   }
 };
 
