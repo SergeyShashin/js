295555f0735402642fb259b0c88d61946277da7b
SergeyShashin <s9997910@yandex.ru>
2025-08-30 22:25:38 +0300

    JS. Advanced. Lesson1. Menu 300825 done.

---
+++ продвинутый курс JavaScript/lessons/lesson1/menu300825/menu.js
@@ -0,0 +1,54 @@
+function Container(userId, userClass) {
+  this.userId = userId;
+  this.userClass = userClass;
+}
+
+Container.prototype.render = function () {
+  var divEl = document.createElement('div');
+
+  divEl.id = this.userId;
+  divEl.className = this.userClass;
+
+  return div
+};
+
+function Menu(userId, userClass, items) {
+  Container.call(this, userId, userClass)
+  this.items = items;
+}
+
+Menu.prototype = Object.create(Container.prototype);
+Menu.prototype.render = function () {
+  var ulEl = document.createElement('ul');
+  ulEl.id = this.userId;
+  ulEl.className = this.userClass
+
+  this.items.forEach(function (item) {
+    if (item instanceof Container) {
+      ulEl.appendChild(item.render());
+    }
+  });
+
+  return ulEl;
+};
+
+
+function MenuItem(href, label) {
+  Container.call(this, '', '')
+  this.href = href;
+  this.label = label;
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+MenuItem.prototype.render = function () {
+  var liEl = document.createElement('li');
+  var aEl = document.createElement('a');
+  liEl.id = this.userId;
+  liEl.className = this.userClass;
+  aEl.href = this.href;
+  aEl.textContent = this.label;
+  liEl.appendChild(aEl);
+
+  return liEl;
+}
+
