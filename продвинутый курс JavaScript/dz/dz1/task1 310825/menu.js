ff94b914e2bae7d18267f1d8540b636a3a58114a
SergeyShashin <s9997910@yandex.ru>
2025-08-31 15:14:25 +0300

    task 310825 done.

---
+++ продвинутый курс JavaScript/dz/dz1/task1 310825/menu.js
@@ -0,0 +1,60 @@
+// 1. Улучшить базовый класс, добавив в него общий для всех метод remove(), который удаляет контейнер.
+
+function Container(userId, userClass) {
+  this.userId = userId;
+  this.userClass = userClass;
+}
+
+Container.prototype.render = function () {
+  var divEl = document.createElement('div');
+
+  divEl.id = this.userId;
+  divEl.className = this.userClass;
+
+  return div
+};
+
+Container.prototype.remove = function () {
+  document.getElementById(this.userId).remove();
+};
+
+function Menu(userId, userClass, items) {
+  Container.call(this, userId, userClass)
+  this.items = items;
+}
+
+Menu.prototype = Object.create(Container.prototype);
+Menu.prototype.render = function () {
+  var ulEl = document.createElement('ul');
+  ulEl.id = this.userId;
+  ulEl.className = this.userClass
+
+  this.items.forEach(function (item) {
+    if (item instanceof Container) {
+      ulEl.appendChild(item.render());
+    }
+  });
+
+  return ulEl;
+};
+
+
+function MenuItem(href, label) {
+  Container.call(this, '', '')
+  this.href = href;
+  this.label = label;
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+MenuItem.prototype.render = function () {
+  var liEl = document.createElement('li');
+  var aEl = document.createElement('a');
+  liEl.id = this.userId;
+  liEl.className = this.userClass;
+  aEl.href = this.href;
+  aEl.textContent = this.label;
+  liEl.appendChild(aEl);
+
+  return liEl;
+}
+
