340b21584d30446630fa4bca356e5f351c15608c
SergeyShashin <s9997910@yandex.ru>
2025-09-07 22:38:54 +0300

    JS. Advanced. DZ3 task3 070925 start.

---
+++ продвинутый курс JavaScript/dz/dz3/task3 070925/validation.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/*
+3. Создать форму обратной связи с полями: Имя, Телефон, e-mail, текст, кнопка «Отправить».
+** - При нажатии на кнопку «Отправить» произвести валидацию полей следующим образом:
+
+- Имя содержит только буквы;
+
+** - Телефон подчиняется шаблону +7(000)000-0000;**
+
+** - E-mail выглядит как mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru**
+
+** - Текст произвольный;**
+** - В случае не прохождения валидации одним из полей необходимо выделять это поле красной рамкой и сообщать пользователю об ошибке.**
+*/
+
+var formEl = document.getElementById('formContacts');
+var rules = {
+  name: '[a-zа-яё]+',
+  phone: '^\\+\d\(\d{3}\)\d{3}\-\d{4}$',
+  email: '^\w+\@\w+\.\w{2}$'
+};
+
+formEl.addEventListener('submit', function (e) {
+  var resultValidation = true;
+  for (let key of Object.keys(rules)) {
+    var rgxp = new RegExp(rules[key], 'ig');
+    if (!rgxp.test(this[key].value)) {
+      resultValidation = false;
+      e.preventDefault();
+    } else {
+      resultValidation = true;
+    }
+  }
+
+  if (resultValidation) {
+    console.log('Форма отправлена.');
+  }
+})
