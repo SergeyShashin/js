2ccc090ade5f8d32d0dfe6b570e81384a9cad3d4
SergeyShashin <s9997910@yandex.ru>
2025-11-16 22:40:03 +0300

    JS. Basic. DZ2 4 task 161125 done.

---
+++ базовый курс JavaScript/dz/dz2/4 task 161125/4-basic-arithmetic-operations.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/*
+Реализовать основные 4 арифметические операции (+, -, /, *) в виде функций с двумя
+параметрами.
+Обязательно использовать оператор return.
+*/
+let a = 8;
+
+let functions = [sum(a, a), difference(a, a), divide(a, a), multiplication(a, a)];
+
+for (let operation of functions) {
+  console.log(operation);
+}
+
+function sum(a, b) {
+  return a + b
+}
+
+function difference(a, b) {
+  return a - b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function multiplication(a, b) {
+  return a * b
+}
+
