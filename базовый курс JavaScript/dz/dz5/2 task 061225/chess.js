ff05bdd8faffbb59fe260f266d0780743e9e32e4
SergeyShashin <s9997910@yandex.ru>
2025-12-06 22:33:02 +0300

    JS. Base. DZ task2 061225 done.

--- базовый курс JavaScript/dz/dz5/2 task 061225/chess.js
+++ базовый курс JavaScript/dz/dz5/2 task 061225/chess.js
@@ -27,18 +27,75 @@ const chess = {
     this.figures = {
       'kw': {
         startPosition: 'E1',
+        position: '',
         code: 'U+2654',
-        HTMLCode: '&#9812;'
+        HTMLCode: '&#9812;',
       },
       'fw': {
         startPosition: 'D1',
+        position: '',
         code: 'U+2655',
         HTMLCode: '&#9813;'
       },
       'rw': {
         startPosition: ['A1', 'H1'],
+        position: '',
         code: 'U+2656',
         HTMLCode: '&#9814;'
+      },
+      'ew': {
+        startPosition: ['C1', 'F1'],
+        position: '',
+        code: 'U+2657',
+        HTMLCode: '&#9815;'
+      },
+      'hw': {
+        startPosition: ['B1', 'G1'],
+        position: '',
+        code: 'U+2658',
+        HTMLCode: '&#9816;'
+      },
+      'pw': {
+        startPosition: this.getStartPositionPaw(2),
+        position: '',
+        code: 'U+2659',
+        HTMLCode: '&#9817;'
+      },
+      'kb': {
+        startPosition: 'E8',
+        position: '',
+        code: 'U+265A',
+        HTMLCode: '&#9818;',
+      },
+      'fb': {
+        startPosition: 'D8',
+        position: '',
+        code: 'U+265B',
+        HTMLCode: '&#9819;'
+      },
+      'rb': {
+        startPosition: ['A8', 'H8'],
+        position: '',
+        code: 'U+265C',
+        HTMLCode: '&#9820;'
+      },
+      'eb': {
+        startPosition: ['C8', 'F8'],
+        position: '',
+        code: 'U+265D',
+        HTMLCode: '&#9821;'
+      },
+      'hb': {
+        startPosition: ['B8', 'G8'],
+        position: '',
+        code: 'U+265E',
+        HTMLCode: '&#9822;'
+      },
+      'pb': {
+        startPosition: this.getStartPositionPaw(7),
+        position: '',
+        code: 'U+265F',
+        HTMLCode: '&#9823;'
       },
     };
     this.setStartPositionFiguresOnChessHTMLEl();
@@ -92,11 +149,12 @@ const chess = {
   setStartPositionFiguresOnChessHTMLEl() {
     for (let key in this.figures) {
       let { startPosition, HTMLCode } = this.figures[key];
-
+      
       if (Array.isArray(startPosition)) {
         for (let position of startPosition) {
           this.setFigure(position, HTMLCode);
         }
+
       } else {
         this.setFigure(startPosition, HTMLCode);
       }
@@ -104,6 +162,13 @@ const chess = {
   },
   setFigure(startPosition, HTMLCode) {
     this.chessBoard[startPosition].innerHTML = HTMLCode;
+  },
+  getStartPositionPaw(num) {
+    let res = [];
+    for (let i = 1; i < this.charsForRow.length - 1; i++) {
+      res.push(this.charsForRow[i] + num);
+    }
+    return res
   }
 };
 
