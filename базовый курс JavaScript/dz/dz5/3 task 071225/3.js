5c4afd472b29774b3633d057c3a45b14d3227f4e
SergeyShashin <s9997910@yandex.ru>
2025-12-07 20:41:57 +0300

    JS. Base. DZ5 task3 071225 done.

--- базовый курс JavaScript/dz/dz5/3 task 071225/3.js
+++ базовый курс JavaScript/dz/dz5/3 task 071225/3.js
@@ -25,11 +25,51 @@ javascript.
 
 const validation = {
   inputHTMLElements: null,
+  rules: null,
+  methods: null,
   run() {
+    // console.log(document.querySelector('[name="password"]').value);
     this.init();
   },
 
   init() {
+    this.rules = {
+      name: [
+        { length: ['>', 0] },
+        { length: ['<', 51] },
+      ],
+      phone: [
+        { length: ['===', 11] },
+        { type: ['number'] }
+      ],
+      password: [
+        { length: ['>', 4] },
+        { length: ['<', 51] },
+      ],
+      passwordRepeat: [
+        { equal: ['password'] }
+      ],
+    };
+
+    this.methods = {
+      length(content, sign, number) {
+        switch (sign) {
+          case '>':
+            return content.length > number ? '' : `Ожидается значение больше ${number}.`;
+          case '<':
+            return content.length < number ? '' : `Ожидается значение меньше ${number}.`;
+          case '===':
+            return content.length === number ? '' : `Ожидается значение равное ${number}.`;
+        }
+      },
+      type(content, type) {
+        return typeof Number(content) === type ? '' : `Ожидается значение c типом ${type}.`;
+      },
+      equal(content, inputName) {
+        return content === document.querySelector(`[name=${inputName}]`).value ? '' : `Ожидается значение равное тому, что напечатали в поле ${inputName}.`
+      }
+
+    };
     this.initHandlers();
   },
 
@@ -38,11 +78,26 @@ const validation = {
   },
 
   handlerClick(e) {
-    e.preventDefault();
     if (e.target.type === 'submit') {
+      let result = [];
       this.inputHTMLElements = document.querySelectorAll('input');
+
       for (let input of this.inputHTMLElements) {
-        console.log(input.name);
+        let rules = this.rules[input.name];
+        let content = input.value;
+        for (let rule of rules) {
+          for (let key in rule) {
+            let msg = this.methods[key](content, rule[key][0], rule[key][1]);
+            msg === '' ? '' : result.push(msg);
+          }
+        }
+      }
+
+      if (result.length > 0) {
+        e.preventDefault();
+        result.forEach(error => console.error(error))
+      } else {
+        alert('Данные отправлены)');
       }
     }
   }
