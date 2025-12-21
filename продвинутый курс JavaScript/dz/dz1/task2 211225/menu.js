f2af703aa8dfbf35758b0f7000322356e0595f0e
SergeyShashin <s9997910@yandex.ru>
2025-12-21 16:17:56 +0300

    JS. Advanced. DZ1 task2 211225 start.

---
+++ продвинутый курс JavaScript/dz/dz1/task2 211225/menu.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// 2. Создать наследника класса Menu – новый класс должен уметь строить меню со вложенными пунктами,
+//  т.е с подменю. Подсказка: главный секрет – в обходе объекта пунктов меню и проверке типов.
+
+function Container(idText, classText) {
+  this.idText = idText;
+  this.classText = classText;
+}
+
+Container.prototype.render = function () {
+  var containerHTMLEl = document.createElement('div');
+  containerHTMLEl.id = this.idText;
+  containerHTMLEl.className = this.classText;
+
+  return containerHTMLEl;
+};
+
+Container.prototype.remove = function () {
+  document.getElementById(this.idText).remove();
+};
+
+function Menu(idText, classText, items) {
+  Container.call(this, idText, classText);
+  this.items = items;
+}
+
+Menu.prototype = Object.create(Container.prototype);
+Menu.prototype.render = function () {
+  var menuHTMLEl = document.createElement('ul');
+  menuHTMLEl.id = this.idText;
+  menuHTMLEl.className = this.classText;
+
+  this.items.forEach(el => {
+    if (el instanceof Container) {
+      menuHTMLEl.appendChild(el.render());
+    }
+  });
+
+  return menuHTMLEl;
+};
+
+function MenuItem(href, label) {
+  Container.call(this, 'idItem', 'classItem');
+  this.href = href;
+  this.label = label;
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+MenuItem.prototype.render = function () {
+  var itemHTMLEl = document.createElement('li');
+  var linkHTMLEl = document.createElement('a');
+
+  itemHTMLEl.id = this.idText;
+  itemHTMLEl.className = this.classText;
+  linkHTMLEl.href = this.href;
+  linkHTMLEl.textContent = this.label;
+
+  itemHTMLEl.appendChild(linkHTMLEl);
+
+  return itemHTMLEl;
+};
