b652d8f6e1b4a3b424e9846498dbffa1a6479556
SergeyShashin <s9997910@yandex.ru>
2025-12-24 23:56:35 +0300

    JS. Advanced. DZ1 task3 231215 updated.

--- продвинутый курс JavaScript/dz/dz1/task3 231225/burger.js
+++ продвинутый курс JavaScript/dz/dz1/task3 231225/burger.js
@@ -12,3 +12,84 @@
 Напишите программу, рассчитывающую стоимость и калорийность гамбургера. Используйте ООП-подход 
 (подсказка: нужен класс Гамбургер, константы, методы для выбора опций и расчета нужных величин).
 */
+
+function Burger(size, stuffing) {
+  this.size = size;
+  this.stuffing = stuffing;
+  price = 0;
+  calories = 0;
+  toppings = [];
+
+  switch (this.size) {
+    case 'big':
+      this.price += 100;
+      this.calories += 40;
+      break;
+    case 'small':
+      this.price += 50;
+      this.calories += 20;
+      break;
+
+    default:
+      throw new BurgerExeption(`Что-то с размером. ${this.size}`);
+  }
+
+  switch (this.stuffing) {
+    case Burger.CHEESE:
+      this.price += 10;
+      this.calories += 20;
+      break;
+    case Burger.SALAD:
+      this.price += 20;
+      this.calories += 5;
+      break;
+    case Burger.POTATO:
+      this.price += 15;
+      this.calories += 10;
+      break;
+
+    default:
+      throw new BurgerExeption(`Что-то с начинкой. ${this.stuffing}`);
+  }
+
+}
+
+Burger.prototype.getPrice = function () {
+  return this.price;
+}
+
+Burger.prototype.getCalories = function () {
+  return this.calories;
+}
+
+Burger.prototype.addToppong = function (topping) {
+  if (!this.toppings.includes(topping)) {
+    this.toppings.push(topping);
+    this.toppings.map(el => {
+      switch (el) {
+        case Burger.SPICES:
+          this.price += 15;
+          break;
+        case Burger.MAYO:
+          this.price += 20;
+          this.calories += 5;
+          break;
+
+        default:
+          throw new BurgerExeption(`Что-то с начинкой. ${this.stuffing}`);
+      }
+    })
+  }
+}
+
+
+function BurgerExeption(msg) {
+  return msg
+}
+
+Burger.CHEESE = 0;
+Burger.SALAD = 1;
+Burger.POTATO = 2;
+
+Burger.SPICES = 0;
+Burger.MAYO = 1;
