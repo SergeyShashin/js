e2726344b90e474b960848040c300857ba7f65bc
SergeyShashin <s9997910@yandex.ru>
2026-01-12 22:55:01 +0300

    JS. Advanced. DZ7 task1 110126 updated.

--- продвинутый курс JavaScript/dz/dz7/task1 110126/gulpfile.js
+++ продвинутый курс JavaScript/dz/dz7/task1 110126/gulpfile.js
@@ -9,4 +9,8 @@ gulp.task('scss', function () {
   return gulp.src('src/scss/**/*.scss')
     .pipe(scss())
     .pipe(gulp.dest('src/css/'));
-})
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/scss/**/*.scss', gulp.parallel(['scss']));
+});
